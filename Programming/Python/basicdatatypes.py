c7eb080dc7f6337d99bdd58a2025f2a8fdf7ee03
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2022-06-08 10:31:09 +0300

    Create basicdatatypes.py

---
+++ Programming/Python/basicdatatypes.py
@@ -0,0 +1,20 @@
+# You know the drill, download and install anaconda to be a tidy programmer.
+# Python 3.8.8 or the latest works fine.
+
+# This script is all about datatypes and built-in functions.
+
+# In Python 3, there is effectively no limit to how long an integer value can be.
+
+decimal_int = 234123541234512534139847598012374587012903742134
+
+# we can use binary, octal or decimal. All of them are integers for type() function.
+
+# 0b (zero + lowercase letter 'b')	Binary 	2
+# 0o (zero + lowercase letter 'o') Octal 	8
+# 0x (zero + lowercase letter 'x') 	Hexadecimal 	16 	
+
+binary_int =  0b10
+octal_int = 0o10
+hexadecimal_int = 0x1C
+
+# Will continue tomorrow.
