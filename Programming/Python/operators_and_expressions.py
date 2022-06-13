f752680d2597a9f44050ffcb47408de80c55a160
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2022-06-13 10:43:38 +0300

    Update operators_and_expressions.py

--- Programming/Python/operators_and_expressions.py
+++ Programming/Python/operators_and_expressions.py
@@ -1,1 +1,48 @@
+# By the end of this, you will be able to create complex expressions by combining objects and operators.
 
+a = 20
+b = 30
+
+a + b - 5 
+
+# A sequence of operands and operators, like a + b - 5, is called an expression.
+# Python supports many operators for combining data objects into expressions.
+
+a = 4
+b = 3
+
+# Unary Positive
++a 
+# 4
+
+# Unary Negation
+-b
+# -3
+
+# Addition
+a + b
+# 7
+
+# Subtraction
+a - b
+# 1
+
+# Multiplication
+a * b
+# 12
+
+# Division
+a / b
+# 1.3333333333333333
+
+# Modulo
+a % b
+# 1
+
+# Floor Division
+a // b
+# 1
+
+# Exponentiation
+a ** b
+# 64
