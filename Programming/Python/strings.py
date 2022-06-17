8a69a73d82e9307a35b2dad9fc8e6d0360940f52
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2022-06-17 10:50:10 +0300

    Update strings.py

--- Programming/Python/strings.py
+++ Programming/Python/strings.py
@@ -32,3 +32,42 @@ in_string in 'That\'s good for now.'
 # str() 	             Returns a string representation of an object
 
 
+# String Indexing
+
+string = "repetition is the key."
+
+string[0]
+
+# r
+
+string[4]
+
+# t
+
+# String Slicing
+# This also works in lists, so get it right.
+
+another_string = "dreams do come true"
+
+another_string[1:4]
+# ream
+
+another_string[0:7]
+another_string[:7]
+# dreams d
+# Same result
+
+# Built-in String Methods
+# Case Conversion
+
+
+s = 'foO BaR BAZ quX'
+#  First character converted to uppercase and all other characters converted to lowercase.
+s.capitalize()
+#  'Foo bar baz qux'
+
+
+
+
+
+
