763293e741d2deed37a5cba56e84801fd2c57493
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2022-06-17 10:16:59 +0300

    Update strings.py

--- Programming/Python/strings.py
+++ Programming/Python/strings.py
@@ -1,1 +1,34 @@
+# This script is all abouut strings & character operations.
+
+string = "gratitude"
+another_string = "empathy"
+and_another_string = "happiness"
+
+best_things_in life = string + " " + another_string + " " + and_another_string 
+
+# gratitude empathy happiness
+
+multiplication = "Can you do this?"
+
+result = multiplication * 4
+
+# Can you do this?Can you do this?Can you do this?Can you do this?
+
+in_string = 'food'
+
+in_string in 'That\'s food for thought.'
+# True
+
+in_string in 'That\'s good for now.'
+# False
+
+
+# Built-in String Functions
+
+# Function 	           #Description
+# chr() 	             Converts an integer to a character
+# ord() 	             Converts a character to an integer
+# len() 	             Returns the length of a string
+# str() 	             Returns a string representation of an object
+
 
