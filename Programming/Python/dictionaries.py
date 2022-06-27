6fd0712a0aef3df91b90be07561e99f1622f5e48
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2022-06-27 09:52:57 +0300

    Update dictionaries.py

--- Programming/Python/dictionaries.py
+++ Programming/Python/dictionaries.py
@@ -29,7 +29,7 @@ MLB_team[1]
 #  File "<pyshell#13>", line 1, in <module>
 #    MLB_team[1]
 
-# Use the Keys
+>>> 'Toronto' not in MLB_team# Use the Keys
 MLB_team["Colorado"]
 # "Rockies"
 
@@ -44,4 +44,65 @@ del MLB_team['Seattle']
 MLB_team
 # {'Colorado': 'Rockies', 'Boston': 'Red Sox', 'Minnesota': 'Twins',
 # 'Milwaukee': 'Brewers', 'Kansas City': 'Royals'}
+
+# You cannot use index number to access the elements of the dictionary.
+# Dictionary Keys vs. List Indices
+
+# Cannot do:
+# MLB_team[1]
+
+# You can do all sort of things
+person = {}
+type(person)
+# <class 'dict'>
+
+person['fname'] = 'Joe'
+person['lname'] = 'Fonebone'
+person['age'] = 51
+person['spouse'] = 'Edna'
+person['children'] = ['Ralph', 'Betty', 'Joey']
+person['pets'] = {'dog': 'Fido', 'cat': 'Sox'}
+
+
+person
+# {'fname': 'Joe', 'lname': 'Fonebone', 'age': 51, 'spouse': 'Edna',
+# 'children': ['Ralph', 'Betty', 'Joey'], 'pets': {'dog': 'Fido', 'cat': 'Sox'}}
+
+
+# Restrictions on Dictionary Values
+# By contrast, there are no restrictions on dictionary values. Literally none at all. 
+# A dictionary value can be any type of object Python supports, including mutable types like
+# lists and dictionaries, and user-defined objects, which you will learn about in upcoming tutorials.
+
+# There is also no restriction against a particular value appearing in a dictionary multiple times:
+
+d = {0: 'a', 1: 'a', 2: 'a', 3: 'a'}
+d
+# {0: 'a', 1: 'a', 2: 'a', 3: 'a'}
+d[0] == d[1] == d[2]
+# True
+
+# in and not works
+
+"Milwaukee" in MLB_team
+# True
+'Toronto' in MLB_team
+# False
+'Toronto' in not MLB_team
+# True
+
+# Also len works
+MLB_team = {
+    'Colorado' : 'Rockies',
+    'Boston'   : 'Red Sox',
+    'Minnesota': 'Twins',
+    'Milwaukee': 'Brewers',
+    'Seattle'  : 'Mariners'
+}
+
+len(MLB_team)
+# 5
+
+
+
 
