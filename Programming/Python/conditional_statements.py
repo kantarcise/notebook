e43d25be6889471bf916e00dfa04ca5d7990ec53
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2022-06-29 11:23:48 +0300

    Update conditional_statements.py

--- Programming/Python/conditional_statements.py
+++ Programming/Python/conditional_statements.py
@@ -1,1 +1,2 @@
-
+# In a Python program, the if statement is how you perform this sort of decision-making. 
+# It allows for conditional execution of a statement or group of statements based on the value of an expression.
