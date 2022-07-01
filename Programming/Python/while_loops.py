e1e540d1c4ecbb40887120137b50763ba3b617e8
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2022-07-01 09:45:06 +0300

    Create while_loops.py

---
+++ Programming/Python/while_loops.py
@@ -0,0 +1,4 @@
+# In this tutorial, you’ll:
+#  Learn about the while loop, the Python control structure used for indefinite iteration
+#  See how to break out of a loop or loop iteration prematurely
+#  Explore infinite loops
