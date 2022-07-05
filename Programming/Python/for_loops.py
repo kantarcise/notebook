9e5330b08c0c4981af22454d306256591a94af01
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2022-07-05 09:35:50 +0300

    Create for_loops.py

---
+++ Programming/Python/for_loops.py
@@ -0,0 +1,5 @@
+# In this file:
+
+#    You’ll start with a comparison of some different paradigms used by programming languages to implement definite iteration.
+#    Then you will learn about iterables and iterators, two concepts that form the basis of definite iteration in Python.
+#    Finally, you’ll tie it all together and learn about Python’s for loops.
