e1013662fe3707fda7c005f25b29ad2a218b416a
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2022-07-19 09:06:42 +0300

    Create basic_io_and_string_formatting.py

---
+++ Programming/Python/basic_io_and_string_formatting.py
@@ -0,0 +1,4 @@
+# For a program to be useful, it usually needs to communicate
+# with the outside world by obtaining input data from the user 
+# and displaying result data back to the user. 
+# In this tutorial, you’ll learn about Python input and output.
