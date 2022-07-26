dd0dadddf8c712df1f5a71923202053c32b9a31a
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2022-07-26 11:31:02 +0300

    Update functions.py

--- Programming/Python/functions.py
+++ Programming/Python/functions.py
@@ -3,3 +3,21 @@
 # Mechanisms for passing arguments to your function
 # How to return data from your function back to the calling environment
 
+# Here is a unusual intro:
+
+# In life, you do this sort of thing all the time, even if you don’t explicitly think of it that way. If you wanted to move 
+# some shelves full of stuff from one side of your garage to the other, then you 
+# hopefully wouldn’t just stand there and aimlessly think, 
+# “Oh, geez. I need to move all that stuff over there! How do I do that???”
+# You’d divide the job into manageable steps:
+
+#     Take all the stuff off the shelves.
+#     Take the shelves apart.
+#    Carry the shelf parts across the garage to the new location.
+#     Re-assemble the shelves.
+#     Carry the stuff across the garage.
+#     Put the stuff back on the shelves.
+
+# Breaking a large task into smaller, bite-sized sub-tasks helps 
+# make the large task easier to think about and manage. As programs become more 
+# complicated, it becomes increasingly beneficial to modularize them in this way.
