aa19642c788a374b09e811113a7649bc423c3a82
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2022-07-26 09:26:57 +0300

    Create functions.py

---
+++ Programming/Python/functions.py
@@ -0,0 +1,5 @@
+# How functions work in Python and why they’re beneficial
+# How to define and call your own Python function
+# Mechanisms for passing arguments to your function
+# How to return data from your function back to the calling environment
+
