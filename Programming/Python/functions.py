61a6d03abcedd4b10cbf8ec4462940dbcd610928
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2022-07-28 10:23:01 +0300

    Update functions.py

--- Programming/Python/functions.py
+++ Programming/Python/functions.py
@@ -53,5 +53,55 @@ f(item='bananas', price=1.74, qty=6)
 def f(qty=6, item='bananas', price=1.74):
     print(f'{qty} {item} cost ${price:.2f}')
 
-    
+  
+# In summary:
+
+#  Positional arguments must agree in order and number with the parameters declared in the function definition.
+#  Keyword arguments must agree with declared parameters in number, but they may be specified in arbitrary order.
+#  Default parameters allow some arguments to be omitted when the function is called.
+
+
+
+# Argument Passing Summary
+
+# Python’s argument-passing mechanism has been called pass-by-assignment. 
+
+# Passing an immutable object, like an int, str, tuple, or frozenset, to 
+# a Python function acts like pass-by-value. The function can’t modify the object in the calling environment.
+
+
+
+def f(fx):
+    print('fx =', fx, '/ id(fx) = ', id(fx))
+    fx = 10
+    print('fx =', fx, '/ id(fx) = ', id(fx))
+
+x = 5
+print('x =', x, '/ id(x) = ', id(x))
+# x = 5 / id(x) =  1357924048
+
+f(x)
+
+# fx = 5 / id(fx) =  1357924048
+# fx = 10 / id(fx) =  1357924128
+
+print('x =', x, '/ id(x) = ', id(x))
+# x = 5 / id(x) =  1357924048
+
+
+
+
+# Passing a mutable object such as a list, dict, or set acts somewhat—but not exactly—like pass-by-reference. The
+# function can’t reassign the object wholesale, but it can change items in place within the object, and
+# these changes will be reflected in the calling environment.
+
+def f(x):
+    x[0] = '---'
+
+my_list = ['foo', 'bar', 'baz', 'qux']
+
+f(my_list)
+my_list
+# ['---', 'bar', 'baz', 'qux']
+
 
