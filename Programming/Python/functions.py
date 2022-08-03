af9ff0a3430d06cd16e72e2503f3f9bfac02efe9
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2022-08-03 09:11:44 +0300

    Update functions.py

--- Programming/Python/functions.py
+++ Programming/Python/functions.py
@@ -249,3 +249,16 @@ f(foo=1, bar=2, baz=3)
 # a dictionary that the function can reference by the name kwargs. 
 
 
+# We can all use them together.
+
+def f(a, b, *args, **kwargs):
+    print(F'a = {a}')
+    print(F'b = {b}')
+    print(F'args = {args}')
+    print(F'kwargs = {kwargs}')
+
+f(1, 2, 'foo', 'bar', 'baz', 'qux', x=100, y=200, z=300)
+# a = 1
+# b = 2
+# args = ('foo', 'bar', 'baz', 'qux')
+# kwargs = {'x': 100, 'y': 200, 'z': 300}
