7b6683f1ce54c7f4f5e8d4b0b957bc414908d601
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2022-08-04 09:31:32 +0300

    Create regexes.py

---
+++ Programming/Python/regexes.py
@@ -0,0 +1,5 @@
+# Rather than searching for a fixed substring like '123', 
+# suppose you wanted to determine whether a string contains any three consecutive
+# decimal digit characters, as in the strings 'foo123bar', 'foo456bar', '234baz', and 'qux678'.
+
+# Strict character comparisons won’t cut it here. This is where regexes in Python come to the rescue.
