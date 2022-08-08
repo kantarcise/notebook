217233121b8db405e41653bfa312df8277a526b2
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2022-08-08 09:43:06 +0300

    Update namespaces_and_scope.py

--- Programming/Python/namespaces_and_scope.py
+++ Programming/Python/namespaces_and_scope.py
@@ -14,3 +14,45 @@
 #     Local
 
 # These have differing lifetimes. As Python executes a program, it creates namespaces as necessary and deletes them when they’re no longer needed.
+
+# The Built-In Namespace
+
+# The built-in namespace contains the names of all of Python’s built-in objects. These are available at all times when Python is running.
+
+
+# dir(__builtins__)
+
+# ['ArithmeticError', 'AssertionError', 'AttributeError',
+#  'BaseException','BlockingIOError', 'BrokenPipeError', 'BufferError',
+#  'BytesWarning', 'ChildProcessError', 'ConnectionAbortedError',
+#  'ConnectionError', 'ConnectionRefusedError', 'ConnectionResetError',
+#  'DeprecationWarning', 'EOFError', 'Ellipsis', 'EnvironmentError',
+#  'Exception', 'False', 'FileExistsError', 'FileNotFoundError',
+#  'FloatingPointError', 'FutureWarning', 'GeneratorExit', 'IOError',
+#  'ImportError', 'ImportWarning', 'IndentationError', 'IndexError',
+#  'InterruptedError', 'IsADirectoryError', 'KeyError', 'KeyboardInterrupt',
+#  'LookupError', 'MemoryError', 'ModuleNotFoundError', 'NameError', 'None',
+#  'NotADirectoryError', 'NotImplemented', 'NotImplementedError', 'OSError',
+#  'OverflowError', 'PendingDeprecationWarning', 'PermissionError',
+#  'ProcessLookupError', 'RecursionError', 'ReferenceError', 'ResourceWarning',
+#  'RuntimeError', 'RuntimeWarning', 'StopAsyncIteration', 'StopIteration',
+#  'SyntaxError', 'SyntaxWarning', 'SystemError', 'SystemExit', 'TabError',
+#  'TimeoutError', 'True', 'TypeError', 'UnboundLocalError',
+#  'UnicodeDecodeError', 'UnicodeEncodeError', 'UnicodeError',
+#  'UnicodeTranslateError', 'UnicodeWarning', 'UserWarning', 'ValueError',
+#  'Warning', 'ZeroDivisionError', '_', '__build_class__', '__debug__',
+#  '__doc__', '__import__', '__loader__', '__name__', '__package__',
+#  '__spec__', 'abs', 'all', 'any', 'ascii', 'bin', 'bool', 'bytearray',
+#  'bytes', 'callable', 'chr', 'classmethod', 'compile', 'complex',
+#  'copyright', 'credits', 'delattr', 'dict', 'dir', 'divmod', 'enumerate',
+#  'eval', 'exec', 'exit', 'filter', 'float', 'format', 'frozenset',
+#  'getattr', 'globals', 'hasattr', 'hash', 'help', 'hex', 'id', 'input',
+#  'int', 'isinstance', 'issubclass', 'iter', 'len', 'license', 'list',
+#  'locals', 'map', 'max', 'memoryview', 'min', 'next', 'object', 'oct',
+#  'open', 'ord', 'pow', 'print', 'property', 'quit', 'range', 'repr',
+#  'reversed', 'round', 'set', 'setattr', 'slice', 'sorted', 'staticmethod',
+#  'str', 'sum', 'super', 'tuple', 'type', 'vars', 'zip']
+
+
+# You’ll see some objects here that you may recognize from previous tutorials—for example, the StopIteration exception,
+# built-in functions like max() and len(), and object types like int and str.
