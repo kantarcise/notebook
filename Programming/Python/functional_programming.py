0052025c1a9753048387d42bda221a1bad2c6e7d
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2022-08-12 11:31:03 +0300

    Update functional_programming.py

--- Programming/Python/functional_programming.py
+++ Programming/Python/functional_programming.py
@@ -1,1 +1,4 @@
-
+# What the functional programming paradigm entails
+# What it means to say that functions are first-class citizens in Python
+# How to define anonymous functions with the lambda keyword
+# How to implement functional code using map(), filter(), and reduce()
