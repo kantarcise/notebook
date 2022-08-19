5fd4c7c943d34320579d3d9ddd1ed00803f18c2c
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2022-08-19 08:11:28 +0300

    Update python_tricks_the_book.py

--- Programming/Python/python_tricks_the_book.py
+++ Programming/Python/python_tricks_the_book.py
@@ -10,3 +10,24 @@ def apply_discount(product,discount):
     price = int(product["price"] * (1 - discount))
     assert 0 <= price <= product["price"]
     return price
+
+# Single Leading Underscore: “_var”
+
+# “Hey, this isn’t really meant to be a part of the
+# public interface of this class. Best to leave it alone.”
+
+class Test:
+    def __init__(self):
+        self.foo = 11
+        self._bar = 23
+
+# Single Trailing Underscore: “var_”
+ 
+# Sometimes the most fitting name for a variable is already taken by a
+# keyword in the Python language. Therefore, names like class or def
+# cannot be used as variable names in Python. In this case, you can
+# append a single underscore to break the naming conflict:
+
+def make_object(name, class_):
+    pass
+
