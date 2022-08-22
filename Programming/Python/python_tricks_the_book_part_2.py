d972ac2331613733895114e0f504ba778a73a07d
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2022-08-22 10:47:47 +0300

    Create python_tricks_the_book_part_2.py

---
+++ Programming/Python/python_tricks_the_book_part_2.py
@@ -0,0 +1,9 @@
+# Effective Functions
+
+#  Python’s Functions Are First-Class
+
+# What is that mean ?
+
+# Python’s functions are first-class objects. You can assign them to variables,
+# store them in data structures, pass them as arguments to other
+# functions, and even return them as values from other functions.
