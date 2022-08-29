0334d8857a7efa6d064bc81d49c5c5300e5c6b7f
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2022-08-29 11:42:35 +0300

    Create python_tricks_part_3.py

---
+++ Programming/Python/python_tricks_part_3.py
@@ -0,0 +1,10 @@
+# Lambdas Are Single-Expression Functions
+
+# The lambda keyword in Python provides a shortcut for declaring
+# small anonymous functions. Lambda functions behave just like
+# regular functions declared with the def keyword. They can be used
+# whenever function objects are required.
+
+add_numbers = lambda x, y: x + y
+add_numbers(5, 3)
+# 8
