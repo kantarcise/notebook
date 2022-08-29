9d77764a0292babea045056e6214ee2ab170cbd9
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2022-08-29 13:31:23 +0300

    Update python_tricks_part_3.py

--- Programming/Python/python_tricks_part_3.py
+++ Programming/Python/python_tricks_part_3.py
@@ -8,3 +8,21 @@
 add_numbers = lambda x, y: x + y
 add_numbers(5, 3)
 # 8
+
+# One difference between lambdas and regular function definitions.
+# Lambda functions are restricted to a single expression. This means a lambda function can’t use statements or
+# annotations—not even a return statement.
+
+# One of the most frequent use case for lambdas is writing
+# short and concise key funcs for sorting iterables by an alternate key:
+
+tuples = [(1, 'd'), (2, 'b'), (4, 'a'), (3, 'c')]
+sorted(tuples, key=lambda x: x[1])
+# [(4, 'a'), (2, 'b'), (3, 'c'), (1, 'd')]
+
+# We've sorted a list of tuples by the second value in each tuple.
+
+sorted(range(-5, 6), key=lambda x: x * x)
+# [0, -1, 1, -2, 2, -3, 3, -4, 4, -5, 5]
+
+# the key basically returns the value after the colon
