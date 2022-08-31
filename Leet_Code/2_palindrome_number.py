6529105d34f89ea47bed8e927cc9b4c702b10532
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2022-08-31 09:55:01 +0300

    Create 2_palindrome_number.py

---
+++ Leet_Code/2_palindrome_number.py
@@ -0,0 +1,14 @@
+# 9. Palindrome Number
+# Easy
+# 
+# Given an integer x, return true if x is palindrome integer.
+# 
+# An integer is a palindrome when it reads the same backward as forward.
+# 
+#    For example, 121 is a palindrome while 123 is not.
+
+# Example 1:
+
+# Input: x = 121
+# Output: true
+# Explanation: 121 reads as 121 from left to right and from right to left.
