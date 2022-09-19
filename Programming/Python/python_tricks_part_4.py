c71bf10bc28aaa29da5e82a224e850ee40577c7c
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2022-09-19 08:58:54 +0300

    Update python_tricks_part_4.py

--- Programming/Python/python_tricks_part_4.py
+++ Programming/Python/python_tricks_part_4.py
@@ -30,5 +30,33 @@ foo('hello', 1, 2, 3, key1='value', key2=999)
 
 # Function Argument Unpacking
 
+# The * and ** operators can be used to “unpack” function 
+# arguments from sequences and dictionaries.
+# Using argument unpacking effectively can help you write more
+# flexible interfaces for your modules and functions.
+
+def print_vector(x, y, z):
+    print('<%s, %s, %s>' % (x, y, z))
+    
+print_vector(0, 1, 0)
+# <0, 1, 0>
+
+# What if our vector is a tuple or list, even a dictionary.
+
+tuple_vec = (1, 0, 1)
+list_vec = [1, 0, 1]
+
+print_vector(tuple_vec[0],
+             tuple_vec[1],
+             tuple_vec[2])
+# <1, 0, 1>
+
+# Thankfully, there’s a better way to handle this situation in Python with
+# Function Argument Unpacking using the * operator:
+
+print_vector(*tuple_vec)
+# <1, 0, 1>
+print_vector(*list_vec)
+# <1, 0, 1>
 
         
