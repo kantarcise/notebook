3ce817bac20cfa83716f7356f971cf5ca45d6ecb
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2022-09-28 09:58:20 +0300

    Update python_tricks_part_5.py

--- Programming/Python/python_tricks_part_5.py
+++ Programming/Python/python_tricks_part_5.py
@@ -1,1 +1,40 @@
+# Returns:
 
+# Python adds an implicit return None statement to the end of any
+# function. Therefore, if a function doesn’t specify a return value, it returns None by default.
+
+# This means you can replace return None statements with bare
+# return statements or even leave them out completely and still get
+# the same result:
+
+def foo1(value):
+    if value:
+        return value
+    else:
+        return None
+
+def foo2(value):
+    """Bare return statement implies `return None`"""
+    if value:
+        return value
+    else:
+        return
+
+def foo3(value):
+    """Missing return statement implies `return None`"""
+    if value:
+        return value
+
+# All three functions properly return None if you pass them a falsy value
+# as the sole argument:
+
+type(foo1(0))
+# <class 'NoneType'>
+
+type(foo2(0))
+# <class 'NoneType'>
+
+type(foo3(0))
+# <class 'NoneType'>
+
+      
