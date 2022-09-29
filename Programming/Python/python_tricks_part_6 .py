2d2e432f4b926eafd0d5b045137ad7ed7ac84f93
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2022-09-29 14:06:08 +0300

    Update python_tricks_part_6 .py

--- Programming/Python/python_tricks_part_6 .py
+++ Programming/Python/python_tricks_part_6 .py
@@ -1,1 +1,46 @@
 # String Conversion (Every Class Needs a __repr__)
+
+
+# When you define a custom class in Python and then try to print one of
+# its instances to the console (or inspect it in an interpreter session), you
+# get a relatively unsatisfying result. The default “to string” conversion
+# behavior is basic and lacks detail:
+
+
+class Car:
+    def __init__(self,color,mileage):
+        self.color = color
+        self.mileage = mileage
+    
+    # added method
+    def __str__(self):
+        return f"A {self.color} car!"
+    
+    # And also added
+    def __repr__(self):
+        return "__repr__ method"
+        
+my_car = Car("red", 28934)
+
+print(my_car)
+# <__console__.Car object at 0x109b73da0>
+
+my_car
+# <__console__.Car object at 0x109b73da0>
+
+# By default all you get is a string containing the class name and the id of
+# the object instance (which is the object’s memory address in CPython.)
+# That’s better than nothing, but it’s also not very useful.
+
+# __str__ is one of Python’s “dunder” (double-underscore) methods
+# and gets called when you try to convert an object into a string through
+# the various means that are available:
+
+# After the added methods of str and repr:
+
+print(my_Car)
+# A red car!
+"{}".format(my_car)
+# "A red car!"
+my_car
+# __repr__ method
