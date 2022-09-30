21fd65c1570483afe94d887483c3212f89b8471e
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2022-09-30 10:27:20 +0300

    Create python_tricks_part_7.py

---
+++ Programming/Python/python_tricks_part_7.py
@@ -0,0 +1,8 @@
+# Defining Your Own Exception Classes
+
+# When I started using Python, I was hesitant to write custom exception
+# classes in my code. But defining your own error types can be of great
+# value. You’ll make potential error cases stand out clearly, and as a
+# result, your functions and modules will become more maintainable.
+
+# You can also use custom error types to provide additional debugging information.
