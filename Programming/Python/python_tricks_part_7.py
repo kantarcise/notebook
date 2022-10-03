4d0f993b8af4b76c5ef51541394662376bac1a0b
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2022-10-03 09:48:08 +0300

    Update python_tricks_part_7.py

--- Programming/Python/python_tricks_part_7.py
+++ Programming/Python/python_tricks_part_7.py
@@ -21,12 +21,53 @@ validate('joe')
 #     raise ValueError
 # ValueError
 
-# This is not really helpful.
+# This is not really helpful. Your teammate can have a hard time with it.
 
 
+class NameTooShortError(ValueError):
+    pass
+
+def validate(name):
+    if len(name) < 10:
+        raise NameTooShortError(name)
+        
+# This is a self documenting exception. So that's better.
+
+# The same is true even if you’re working on a code base all by yourself.
+# A few weeks or months down the road you’ll have a much easier time
+# maintaining your code if it’s well-structured.
 
 
+# !
+# A GOOD PRACTICE
+# !
 
+# Whenever you’re publicly releasing a Python package, or even if you’re
+# creating a reusable module for your company, it’s good practice to 
+# create a custom exception base class for the module and then derive all
+# of your other exceptions from it.
 
+# Here’s how to create a custom exception hierarchy for all exceptions
+# in a module or package. The first step is to declare a base class that
+# all of our concrete errors will inherit from:
+
+class BaseValidationError(ValueError):
+    pass
+
+# Now, all of our “real” error classes can be derived from the base error
+# class. This gives a nice and clean exception hierarchy with little extra effort:
+
+class NameTooShortError(BaseValidationError):
+    pass
+
+class NameTooLongError(BaseValidationError):
+    pass
+
+class NameTooCuteError(BaseValidationError):
+    pass
 
 # Defining your own exception types will state your code’s intent more clearly and make it easier to debug.
+
+# Derive your custom exceptions from Python’s built-in Exception class or from more specific exception classes like ValueError or KeyError.
+
+# You can use inheritance to define logically grouped exception hierarchies.
