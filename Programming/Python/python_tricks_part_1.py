4796f234c4acc08005a2f58735d6e40951587baa
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2022-10-06 10:46:30 +0300

    Asserts, dunders, string formatting.

--- Programming/Python/python_tricks_part_1.py
+++ Programming/Python/python_tricks_part_1.py
@@ -2,9 +2,9 @@
 
 # If your program is bug-free, these conditions will never occur. But if
 # they do occur, the program will crash with an assertion error telling
-# you exactly which “impossible” condition was triggered. This makes
-# it much easier to track down and fix bugs in your programs. And I like
-# anything that makes life easier—don’t you?
+# you exactly which “impossible” condition was triggered. 
+
+# This makes it much easier to track down and fix bugs in your programs.
 
 def apply_discount(product,discount):
     price = int(product["price"] * (1 - discount))
