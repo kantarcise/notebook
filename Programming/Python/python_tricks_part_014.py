1bd32ac5c110687b6ac90e4cc5d4f09830d1c322
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2022-10-26 09:27:01 +0300

    Common Data Structures in Python - Records.

--- Programming/Python/python_tricks_part_014.py
+++ Programming/Python/python_tricks_part_014.py
@@ -84,3 +84,46 @@ car3 = (3431.5, 'green', True, 'silver')
 
 # ------------------------------------------------------
 
+# Writing a Custom Class – More Work, More Control
+
+# Using regular Python classes as record data types is feasible, but it also
+# takes manual work to get the convenience features of other implementations.
+# For example, adding new fields to the __init__ constructor is verbose and takes time.
+
+# Also, the default string representation for objects instantiated from
+# custom classes is not very helpful. To fix that you may have to add
+# your own __repr__ method,18 which again is usually quite verbose
+# and must be updated every time you add a new field.
+
+# Fields stored on classes are mutable, and new fields can be added
+# freely, which you may or may not like. It’s possible to provide more
+# access control and to create read-only fields using the @property decorator,
+# but once again, this requires writing more glue code.
+
+# Writing a custom class is a great option whenever you’d like to add
+# business logic and behavior to your record objects using methods.
+# However, this means that these objects are technically no longer plain data objects.
+
+
+class Car:
+    def __init__(self, color, mileage, automatic):
+        self.color = color
+        self.mileage = mileage
+        self.automatic = automatic
+
+car1 = Car('red', 3812.4, True)
+car2 = Car('blue', 40231.0, False)
+
+# Get the mileage:
+car2.mileage
+# 40231.0
+
+# Classes are mutable:
+car2.mileage = 12
+car2.windshield = 'broken'
+
+# String representation is not very useful
+# (must add a manually written __repr__ method):
+car1
+# <Car object at 0x1081e69e8>
+
