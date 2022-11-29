7620d8eaf702001dc11e126e1061ceee9114b266
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2022-11-29 21:47:43 +0300

    Generators, "Just in time" values. "()" is the cool thing.

--- Programming/Python/python_tricks_part_024.py
+++ Programming/Python/python_tricks_part_024.py
@@ -34,11 +34,108 @@
 iterator = ('Hello' for i in range(3))
 
 # Careful about the paranthesis. it's circular.
-# This is basicallt one line version of this:
+# This is basically one line version of this:
 
 def bounded_repeater(value, max_repeats):
     for i in range(max_repeats):
         yield value
 
 iterator = bounded_repeater('Hello', 3)
+
+
+# Back to what we've learned:
+
+iterator = ("Hello" for i in range(3))
+
+# We can use this generator with: 
+
+for x in iterator:
+    print(x)
+
+# 'Hello'
+# 'Hello'
+# 'Hello'
+
+# Generator Expressions vs List Comprehensions
+
+listcomp = ['Hello' for i in range(3)]
+genexpr = ('Hello' for i in range(3))
+
+# Unlike list comprehensions, however, generator expressions don’t
+# construct list objects. Instead, they generate values “just in time” like
+# a class-based iterator or generator function would.
+
+listcomp
+# ['Hello', 'Hello', 'Hello']
+genexpr
+# <generator object <genexpr> at 0x1036c3200>
+
+# To access the values produced by the generator we need to call next() on it.
+
+next(genexpr)
+# 'Hello'
+next(genexpr)
+# 'Hello'
+next(genexpr)
+# 'Hello'
+next(genexpr)
+# StopIteration
+
+# Also we can call list() function on a generator to construct a list
+
+list(genexpr)
+# ['Hello', 'Hello', 'Hello']
+
+# If you need a list object right away, you’d normally just write a
+# list comprehension from the get-go.
+
+# ---------------
+
+# Generator General Expression
+
+genexpr = (expression for item in collection)
+
+# Which is:
+
+def generator():
+    for item in collection:
+        yield expression
+
+# We can filter values with conditions on generators as well
+
+even_squares = (x * x for x in range(10)
+                if x % 2 == 0)
+
+for x in even_squares:
+    print(x)
+
+# 0
+# 4
+# 16
+# 36
+# 64
+
+# With added if condition:
+genexpr = (expression for item in collection
+           if condition)
+
+# In-line Generator Expressions
+# Because generator expressions are, well...expressions, you can use
+# them in-line with other statements. For example, you can define an
+# iterator and consume it right away with a for -loop:
+
+for x in ('Bom dia' for i in range(3)):
+    print(x)
+
+# Key Takeaways
+
+# • Generator expressions are similar to list comprehensions.
+# However, they don’t construct list objects. Instead, generator
+# expressions generate values “just in time” like a class-based
+# iterator or generator function would.
+# • Once a generator expression has been consumed, it can’t be
+# restarted or reused.
+# • Generator expressions are best for implementing simple “ad
+# hoc” iterators. For complex iterators, it’s better to write a 
+# generator function or a class-based iterator.
 
