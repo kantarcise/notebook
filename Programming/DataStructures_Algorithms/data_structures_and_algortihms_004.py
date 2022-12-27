7cdb44dcd3da24ac0b76f859c8b24f386dd098fb
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2022-12-27 09:10:37 +0300

    Functions Information Passing, Mutable Parameters.

--- Programming/DataStructures_Algorithms/data_structures_and_algortihms_004.py
+++ Programming/DataStructures_Algorithms/data_structures_and_algortihms_004.py
@@ -59,3 +59,58 @@ def contains(data, target):
 # executed and the function immediately ends, with True designating that the target
 # value was found. Conversely, if the for loop reaches its conclusion without ever
 # finding the match, the final return False statement will be executed.
+
+# Information Passing
+
+# To be a successful programmer, one must have clear understanding of the mechanism
+# in which a programming language passes information to and from a function.
+# In the context of a function signature, the identifiers used to describe the
+# expected parameters are known as formal parameters, and the objects sent by the
+# caller when invoking the function are the actual parameters. Parameter passing
+# in Python follows the semantics of the standard assignment statement. When a
+# function is invoked, each identifier that serves as a formal parameter is assigned, in
+# the function’s local scope, to the respective actual parameter that is provided by the
+# caller of the function.
+
+# For example, consider the following call to our count function from line 8:
+
+prizes = count(grades, A )
+
+# Just before the function body is executed, the actual parameters, grades and A ,
+# are implicitly assigned to the formal parameters, data and target, as follows:
+
+data = grades
+target = A
+
+# The communication of a return value from the function back to the caller is
+# similarly implemented as an assignment. Therefore, with our sample invocation of
+# prizes = count(grades, A ), the identifier prizes in the caller’s scope is assigned
+# to the object that is identified as n in the return statement within our function body.
+
+# An advantage to Python’s mechanism for passing information to and from a
+# function is that objects are not copied. This ensures that the invocation of a function
+# is efficient, even in a case where a parameter or return value is a complex object.
+
+# Mutable Parameters
+
+# Python’s parameter passing model has additional implications when a parameter is
+# a mutable object. Because the formal parameter is an alias for the actual parameter,
+# the body of the function may interact with the object in ways that change its state.
+# Considering again our sample invocation of the count function, if the body of the
+# function executes the command data.append( F ), the new entry is added to the
+# end of the list identified as data within the function, which is one and the same as
+# the list known to the caller as grades. As an aside, we note that reassigning a new
+# value to a formal parameter with a function body, such as by setting data = [ ],
+# does not alter the actual parameter; such a reassignment simply breaks the alias.
+
+# Our hypothetical example of a count method that appends a new element to a
+# list lacks common sense. There is no reason to expect such a behavior, and it would
+# be quite a poor design to have such an unexpected effect on the parameter. There
+# are, however, many legitimate cases in which a function may be designed (and
+# clearly documented) to modify the state of a parameter. As a concrete example,
+# we present the following implementation of a method named scale that’s primary
+# purpose is to multiply all entries of a numeric data set by a given factor.
+
+def scale(data, factor):
+    for j in range(len(data)):
+        data[j] = factor
