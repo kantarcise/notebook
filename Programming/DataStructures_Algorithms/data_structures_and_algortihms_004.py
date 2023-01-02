6867913dcf496f6b01b60b394acdc31cc51f11c0
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2023-01-02 08:50:00 +0300

    Python’s Built-In Functions

--- Programming/DataStructures_Algorithms/data_structures_and_algortihms_004.py
+++ Programming/DataStructures_Algorithms/data_structures_and_algortihms_004.py
@@ -179,4 +179,67 @@ def range(start, stop=None, step=1):
 
 # From a technical perspective, when range(n) is invoked, the actual parameter n will
 # be assigned to formal parameter start. Within the body, if only one parameter is
-# received, the start and stop values are reassigned to provide the desired semantics.
+# received, the start and stop values are reassigned to provide the desired semantcs.
+
+# Python’s Built-In Functions
+
+# When choosing names for the parameters, we use identifiers x, y, z for arbitrary numeric
+# types, k for an integer, and a, b, and c for arbitrary comparable types. We use
+# the identifier, iterable, to represent an instance of any iterable type (e.g., str, list,
+# tuple, set, dict).
+
+# A sequence represents a more narrow category of indexable classes, including str,
+# list, and tuple, but neither set nor dict. Most of the entries in Table 1.4 can be
+# categorized according to their functionality as follows:
+
+# Input/Output: print, input, and open.
+
+# Character Encoding: ord and chr relate characters and their integer code points.
+# For example, ord( A ) is 65 and chr(65) is A .
+
+# Mathematics: abs, divmod, pow, round, and sum provide common mathematical
+# functionality.
+
+# Ordering: max and min apply to any data type that supports a notion of 
+# comparison, or to any collection of such values. Likewise, sorted can be used to produce
+# an ordered list of elements drawn from any existing collection.
+
+# Collections/Iterations: range generates a new sequence of numbers; len reports
+# the length of any existing collection; functions reversed, all, any, and map operate
+# on arbitrary iterations as well; iter and next provide a general framework for
+# operation through elements of a collection.
+
+Calling Syntax                                 Description
+abs(x)                                         # Return the absolute value of a number.
+all(iterable)                                  # Return True if bool(e) is True for each element e.
+any(iterable)                                  # Return True if bool(e) is True for at least one element e.
+chr(integer)                                   # Return a one-character string with the given Unicode code point.
+divmod(x, y)                                   # Return (x // y, x % y) as tuple, if x and y are integers.
+hash(obj)                                      # Return an integer hash value for the object (see Chapter 10).
+id(obj)                                        # Return the unique integer serving as an “identity” for the object.
+input(prompt)                                  # Return a string from standard input; the prompt is optional.
+isinstance(obj, cls)                           # Determine if obj is an instance of the class (or a subclass).
+iter(iterable)                                 # Return a new iterator object for the parameter (see Section 1.8).
+len(iterable)                                  # Return the number of elements in the given iteration.
+map(f, iter1, iter2, ...)                      # Return an iterator yielding the result of function calls f(e1, e2, ...)
+                                               # for respective elements e1 ∈ iter1, e2 ∈ iter2, ...
+max(iterable)                                  # Return the largest element of the given iteration.
+max(a, b, c, ...)                              # Return the largest of the arguments.
+min(iterable)                                  # Return the smallest element of the given iteration.
+min(a, b, c, ...)                              # Return the smallest of the arguments.
+next(iterator)                                 # Return the next element reported by the iterator (see Section 1.8).
+open(filename, mode)                           # Open a file with the given name and access mode.
+ord(char)                                      # Return the Unicode code point of the given character.
+pow(x, y)                                      # Return the value x y (as an integer if x and y are integers);
+                                               # equivalent to x y.
+pow(x, y, z)                                   # Return the value (x y mod z) as an integer.
+print(obj1, obj2, ...)                         # Print the arguments, with separating spaces and trailing newline.
+range(stop)                                    # Construct an iteration of values 0, 1, . . . , stop − 1.
+range(start, stop)                             # Construct an iteration of values start, start + 1, . . . , stop − 1.
+range(start, stop, step)                       # Construct an iteration of values start, start + step, start + 2 step, . . .
+reversed(sequence)                             # Return an iteration of the sequence in reverse.
+round(x)                                       #  Return the nearest int value (a tie is broken toward the even value).
+round(x, k)                                    # Return the value rounded to the nearest 10−k (return-type matches x).
+sorted(iterable)                               # Return a list containing elements of the iterable in sorted order.
+sum(iterable)                                  #  Return the sum of the elements in the iterable (must be numeric).
+type(obj)                                      # Return the class to which the instance obj belongs.
