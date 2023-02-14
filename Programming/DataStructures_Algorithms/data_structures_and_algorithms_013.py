15229d18df742724345bd88156bb3fa4cb36faac
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2023-02-14 23:58:37 +0300

    Implementing implied methods with Range, Inheritence with CreditCards

--- Programming/DataStructures_Algorithms/data_structures_and_algorithms_013.py
+++ Programming/DataStructures_Algorithms/data_structures_and_algorithms_013.py
@@ -39,3 +39,164 @@ class Range:
             raise IndexError ("index out of range")
 
         return self._start + k * self._step
+    
+# Inheritance
+
+# A natural way to organize various structural components of a software package
+# is in a hierarchical fashion, with similar abstract definitions grouped together in
+# a level-by-level manner that goes from specific to more general as one traverses
+# up the hierarchy.
+
+# In object-oriented programming, the mechanism for a modular and hierarchical organization
+# is a technique known as inheritance. This allows a new class to be defined
+# based upon an existing class as the starting point. In object-oriented terminology,
+# the existing class is typically described as the base class, parent class, or super-
+# class, while the newly defined class is known as the subclass or child class.
+
+# There are two ways in which a subclass can differentiate itself from its superclass.
+# A subclass may specialize an existing behavior by providing a new implementation
+# that overrides an existing method. A subclass may also extend its superclass by providing brand new methods.
+
+# Here is an example 
+
+# BaseException
+#     +-- SystemExit
+#     +-- KeyboardInterrupt
+#     +-- GeneratorExit
+#     +-- Exception
+#          +-- StopIteration
+#          +-- StandardError
+#          |    +-- BufferError
+#          |    +-- ArithmeticError
+#          |    |    +-- FloatingPointError
+#          |    |    +-- OverflowError
+#          |    |    +-- ZeroDivisionError
+#          |    +-- AssertionError
+#          |    +-- AttributeError
+#          |    +-- EnvironmentError
+#          |    |    +-- IOError
+#          |    |    +-- OSError
+#          |    |         +-- WindowsError (Windows)
+#          |    |         +-- VMSError (VMS)
+#          |    +-- EOFError
+#          |    +-- ImportError
+#          |    +-- LookupError
+#          |    |    +-- IndexError
+#          |    |    +-- KeyError
+#          |    +-- MemoryError
+#          |    +-- NameError
+#          |    |    +-- UnboundLocalError
+#          |    +-- ReferenceError
+#          |    +-- RuntimeError
+#          |    |    +-- NotImplementedError
+#          |    +-- SyntaxError
+#          |    |    +-- IndentationError
+#          |    |         +-- TabError
+#          |    +-- SystemError
+#          |    +-- TypeError
+#          |    +-- ValueError
+#          |         +-- UnicodeError
+#          |              +-- UnicodeDecodeError
+#          |              +-- UnicodeEncodeError
+#          |              +-- UnicodeTranslateError
+#          +-- Warning
+#               +-- DeprecationWarning
+#               +-- PendingDeprecationWarning
+#               +-- RuntimeWarning
+#               +-- SyntaxWarning
+#               +-- UserWarning
+#               +-- FutureWarning
+#           +-- ImportWarning
+#           +-- UnicodeWarning
+#           +-- BytesWarning
+
+# Here is an example for inheritence.
+
+class CreditCard:
+    """A consumer credit card"""
+    def __init__(self, customer, bank, acnt, limit)"
+        """Initalize a new credit card
+        
+        Initial balance is zero
+        
+        customer            the name of the customer        eg. Ali Pek
+        bank                the name of the bank            eg. Bank of America
+        acnt                the account identifier number   eg. 5391 0375 9387 5309
+        limit               the card limit                  eg. $ 1000       
+        balance             the total debt of card          eg. $ 250
+        """
+
+        self._customer = customer
+        self._bank = bank
+        self._acnt = acnt
+        self._limit = limit
+        self._balance = 0
+
+    def get_customer(self):
+        return self._customer
+
+    def get_bank(self):
+        return self._bank   
+
+    def get_account(self):
+        return self._acnt  
+
+    def get_limit(self):
+        return self._limit
+
+    def get_balance(self): 
+        return self._balance    
+
+    def charge(self,price):
+        """Charge given price to the card, assuming sufficient limit
+        
+        Returns True if the charge was successful, False otherwise"""
+
+        if price + self._balance > self._limit:
+            print("Insufficient Limit")
+            return False
+        else:
+            self._balance += price
+            return True
+
+    def make_payment(self, amount):
+        self._balance -= amount
+
+
+
+class DangerousCreditCard(CreditCard):
+    """ An extention to the Credit Card that has compound interest and fees
+    
+    """
+    def __init__(self,customer,bank,acnt,limit, apr):
+        """Initalize a new Dangerous credit card
+        
+        Initial balance is zero
+        
+        customer            the name of the customer        eg. Ali Pek
+        bank                the name of the bank            eg. Bank of America
+        acnt                the account identifier number   eg. 5391 0375 9387 5309
+        limit               the card limit                  eg. $ 1000       
+        balance             the total debt of card          eg. $ 250
+        apr                 the annual percentage rate      eg. 0.0825 for %8.25 APR  
+        """
+        super().__init__(customer,bank,acnt,limit)
+        self._apr = apr
+
+    def charge(self, price):
+        """Charge given price to the card, assuming sufficient credit limit.
+        Return True if charge was processed.
+        Return False and assess 5 fee if charge is denied.
+        """
+        success = super().charge(price)
+        if not success:
+            self._balance -= 5
+        
+        return success
+        
+    def process_month(self):
+        """Asses monthly interest on outstanding balance"""
+        if self._balance > 0:
+            # if positive balance, convert APR to monthly multiplicative factor
+            monthly_factor = pow(1 + self._apr, 1/12)
+            self._balance *= monthly_factor
