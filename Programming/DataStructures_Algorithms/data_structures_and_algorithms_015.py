792b246e5e6e6feac6fc57bc51baa0e2b6f61734
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2023-02-17 13:16:39 +0300

    Abstract Base Classes

--- Programming/DataStructures_Algorithms/data_structures_and_algorithms_015.py
+++ Programming/DataStructures_Algorithms/data_structures_and_algorithms_015.py
@@ -40,5 +40,54 @@
 # sequences that support element access via an integer index. More so, the collections.Sequence class
 # provides concrete implementations of methods, count, index, and contains
 # that can be inherited by any class that provides concrete implementations of both
-# len and getitem . For the purpose of illustration, we provide a sample
-# implementation of such a Sequence abstract base class in Code Fragment 2.14.
+# len and getitem . Lets write a Sequence abc.
+
+
+from abc import ABCMeta, abstractmethod
+# need these definitions
+class Sequence(metaclass=ABCMeta):
+    """Our own version of collections.Sequence abstract base class."""
+
+    @abstractmethod
+    def __len__(self):
+        """Return the length of the sequence."""
+    
+    @abstractmethod
+    def __getitem__(self, j):
+        """Return the element at index j of the sequence."""
+    
+    def __contains__(self, val):
+        """Return True if val found in the sequence; False otherwise."""
+        for j in range(len(self)):
+            if self[j] == val:                              # found match 
+                return True
+        return False
+    
+    def index(self, val):
+        """Return leftmost index at which val is found (or raise ValueError)."""
+        for j in range(len(self)):
+            if self[j] == val:                              # leftmost match
+                return j
+        raise ValueError( "Value not in sequence" )        # never found a match
+    
+    def count(self, val):
+        """Return the number of elements equal to given value."""
+        k = 0
+        for j in range(len(self)):
+            if self[j] == val:                              #found a match
+                k += 1
+        return k
+
+# This implementation relies on two advanced Python techniques. The first is that
+# we declare the ABCMeta class of the abc module as a metaclass of our Sequence
+# class. A metaclass is different from a superclass, in that it provides a template for
+# the class definition itself. Specifically, the ABCMeta declaration assures that the
+# constructor for the class raises an error.
+
+# The second advanced technique is the use of the @abstractmethod decorator
+# immediately before the len and getitem methods are declared. That declares
+# these two particular methods to be abstract, meaning that we do not provide
+# an implementation within our Sequence base class, but that we expect any concrete
+# subclasses to support those two methods. Python enforces this expectation, by disallowing
+# instantiation for any subclass that does not override the abstract methods with concrete implementations.
+
