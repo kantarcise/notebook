9d9b232c11aad939c5b7ad7857d08bba4afbb914
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2023-10-25 22:29:02 +0300

    The Perfect Hour :stopwatch:

--- neetcode/060.serialize_and_deserialize_binary_tree.py
+++ neetcode/060.serialize_and_deserialize_binary_tree.py
@@ -32,6 +32,25 @@ Constraints:
 
 Takeaway:
 
+You can solve the problem with breadth-first search
+or you can solve it with depth first search
+using preorder traversal
+
+for the example tree
+     1
+    / \
+   2   3
+      / \
+     4   5
+
+"1,2,N,N,3,4,N,N,5,N,N"
+is the resulting string
+if we used N for None nodes
+
+For each subtree, check the left and right nodes,
+and recursively go a level up
+
+Also added the Breadth First Search Solution
 
 """
 
@@ -42,7 +61,10 @@ class TreeNode(object):
         self.left = None
         self.right = None
 
-class Codec:
+
+# I TRIED REALLY HARD, BUT THIS WAS NOT ACCEPPTED.
+
+class SezaiCodec:
 
     def serialize(self, root):
         """Encodes a tree to a single string.
@@ -50,8 +72,29 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
-        pass
+        # if root is None 
+        if not root:
+            return ""
+
+        # lets use depth-first search
+        # and implement the tree using an array 
+        # return the string of the array after.
+        # * nodes are for the none nodes
+        tree_as_array = []
+
+        def dfs(node):
+            if not node:
+                tree_as_array.append("*")
+                return
+            
+            tree_as_array.append(str(node.val))
+
+            dfs(node.left)
+            dfs(node.right)
         
+        dfs(root)
+
+        return ",".join(tree_as_array)
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -59,10 +102,188 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
-        pass
+        # if no data, return None 
+        if not data:
+            return None
+
+        nodes = data.split(",")
+
+        def inverse_dfs(nodes_list):
+
+            if not nodes_list:
+                return None
+
+            root = TreeNode(nodes_list[0])
+            for i in range(1,len(nodes_list) - 1):
+                if nodes_list[i] == "*":
+                    return
+                if i % 2 == 0:
+                    root.left = inverse_dfs(nodes_list[i + 1:])
+                if i % 2 == 1:
+                    root.right = inverse_dfs(nodes_list[i + 1:])
+            return root
+
+        tree = inverse_dfs(nodes)
+
+        return tree
         
 
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
 # deser = Codec()
 # ans = deser.deserialize(ser.serialize(root))
+
+# NEETCODE
+class Codec:
+
+    # You can solve the problem with breadth-first search
+    # or you can solve it with depth first search
+    # using preorder traversal
+
+    # for the example tree
+    #      1
+    #     / \
+    #    2   3
+    #       / \
+    #      4   5
+
+    # "1,2,N,N,3,4,N,N,5,N,N"
+    # is the resulting string
+    # if we used N for None nodes
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+
+        result = []
+        # preorder depth-first search
+        def dfs(node):
+            if not node:
+                result.append("N")
+                return
+            result.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+
+        dfs(root)
+        return ",".join(result)        
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+
+        nodes_list = data.split(",")
+        self.i = 0
+
+        def dfs():
+            if nodes_list[self.i] == "N":
+                # onto the next element
+                self.i += 1
+                # return a None node
+                return None
+            node = TreeNode(int(nodes_list[self.i]))
+            self.i += 1
+            # construct the left subtree
+            node.left = dfs()
+            # construct the right subtree
+            node.right = dfs()
+            return node
+        
+        return dfs()
+
+from collections import deque
+
+# Breadth-first Search Solution
+class CodecSecond:
+   
+    def serialize(self, root):
+        if root == None: 
+            # let "#" be None node
+            return "#"
+        
+        # Start by making a deque
+        bfs = deque([root])
+        ans = []
+        while bfs:
+            curr = bfs.popleft()
+            if curr == None:
+                ans.append("#")
+            else:
+                ans.append(str(curr.val))
+                bfs.append(curr.left)
+                bfs.append(curr.right)
+        return ",".join(ans)
+
+    def deserialize(self, data):
+        def make_node(str_value):
+            if str_value == "#":
+                return None
+            return TreeNode(int(str_value))
+
+        if data == "#": 
+            return None
+        
+        node_list = data.split(",")
+        root = make_node(node_list[0])
+        i = 1
+        bfs = deque([root])
+        while bfs:
+            curr = bfs.popleft()
+            # left node
+            curr.left = make_node(node_list[i])
+            # right node
+            curr.right = make_node(node_list[i+1])
+            # move counter 2 
+            i += 2
+
+            if curr.left != None:
+                bfs.append(curr.left)
+            if curr.right != None:
+                bfs.append(curr.right)
+
+        return root
+
+
+
+# Create a binary tree:
+#      1
+#     / \
+#    2   3
+#       / \
+#      4   5
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.right.left = TreeNode(4)
+root.right.right = TreeNode(5)
+
+# Create a Codec instance
+codec = Codec()
+
+# Serialize the tree
+serialized_tree = codec.serialize(root)
+print("Serialized Tree:", serialized_tree)  
+# Output: "1,2,*,*,3,4,*,*,5,*,*"
+
+# Deserialize the tree
+deserialized_tree = codec.deserialize(serialized_tree)
+
+# Check if the deserialized tree is the same as the original tree
+def are_trees_equal(node1, node2):
+    if not node1 and not node2:
+        return True
+    if (node1 and not node2) or (not node1 and node2):
+        return False
+    return (
+        node1.val == node2.val
+        and are_trees_equal(node1.left, node2.left)
+        and are_trees_equal(node1.right, node2.right)
+    )
+
+print("Are Trees Equal:", are_trees_equal(root, deserialized_tree))  # Output: True
--- neetcode/063.word_search_2.py
+++ neetcode/063.word_search_2.py
@@ -33,7 +33,124 @@ Constraints:
 
 Takeaway:
 
+we need to make a data structure where 
+we can see every possible word in the board
+we can use a Trie
+        
+Brute force would be,
+starting from each tile, run a depth first search
+and check if you can make the words
+        
+we can check every word at the same time
+because our main condition is based on prefix
+So a Trie is great!
+
+lets make a Trie for our words
+in order to not check the words list every time 
+we go down in out dfs
+Also we do not have to check tiles that our words 
+does not start with.
+
+Base case for the DFS is pretty big:
+
+out of bounds
+already visited position
+maybe the character we are working 
+on is not in out Trie
+
 """
+
+class TrieNode:
+    # streamline memory usage
+    __slots__ = "children", "end_of_word"
+    def __init__(self):
+        self.children = {}
+        self.end_of_word = False 
+
+    def add_word(self, word):
+        # root node
+        cur = self
+
+        for c in word:
+            # If that character does not exist
+            if c not in cur.children:
+                # make a new Node
+                cur.children[c] = TrieNode()
+            # go on forward
+            cur = cur.children[c]
+        # word ended
+        cur.end_of_word = True
+
 class Solution:
-    def findWords(self, board: "list[list[str]]", words: "list[str]") -> "list[str]":
-        pass
+
+    # first try
+    def find_words(self, board: "list[list[str]]", 
+                words: "list[str]") -> "list[str]":
+        # we need to make a data structure where 
+        # we can see every possible word in the board
+        # we can use a Trie
+        
+        # Brute force would be,
+        # starting from each tile, run a depth first search
+        # and check if you can make the words
+        
+        # we can check every word at the same time
+        # because our main condition is based on prefix
+        # So a Trie is great!
+
+        # lets make a Trie for our words
+        # in order to not check the words list every time 
+        # we go down in out dfs
+        # also we do not have to check tiles that our words 
+        # does not start with 
+        
+        root = TrieNode()
+
+        # add all words to our Trie
+        for w in words:
+            root.add_word(w)
+        
+        ROWS, COLS = len(board), len(board[0])
+        
+        # result - we want the result to be unique
+        # visit - we do not want to repeat same character
+        result, visit = set(), set()
+
+        def dfs(r, c, node, word):
+            # out of bounds
+            # already visited position
+            # maybe the character we are working 
+            # on is not in out Trie
+            if (r < 0 or c < 0 or 
+                r == ROWS or c == COLS or
+                (r, c) in visit or board[r][c] not in node.children):
+                return
+            
+            # add the position to visited
+            visit.add((r,c))
+
+            # onto the next tile
+            node = node.children[board[r][c]]
+            
+            # add the character to our current word
+            word += board[r][c]
+
+            # is the current result a Word?
+            if node.end_of_word:
+                result.add(word)
+
+            # go to every position
+            dfs(r - 1, c, node, word)
+            dfs(r + 1, c, node, word)
+            dfs(r, c - 1, node, word)
+            dfs(r, c + 1, node, word)
+
+            # after we are done, we can remove 
+            # the position from being visited
+            visit.remove((r,c))
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                dfs(r, c, root, "")
+
+        return list(result)
