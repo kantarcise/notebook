a5bc412d0e855f0e72833e3b64c73c596c20d451
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2023-10-27 23:27:15 +0300

    The Perfect Hour :stopwatch:

--- neetcode/076.kth_largest_element_in_an_array.py
+++ neetcode/076.kth_largest_element_in_an_array.py
@@ -24,8 +24,78 @@ Constraints:
 
 Takeaway:
 
+instead of sorting - o(nlogn)
+we can use a heap
+
+every time we pop an element from the heap
+it is log n
+so this result will be - o(n + k log n)
+
+There is a solution whick uses quick_select
+which is kinda like quick_sort
+
 """
+from heapq import heapify, heappop
 
 class Solution:
+
+    def find_kth_largest(self, nums: "list[int]", k: int) -> int:
+        # instead of sorting - o(nlogn)
+        # we can use a heap
+
+        # every time we pop an element from the heap
+        # it is log n
+        # so this result will be - o(n + k log n)
+
+        negative = [-elem for elem in nums]
+        heapify(negative)
+        # now we have a max heap
+        # for [3,2,1,5,6,4] - negative - [-6, -5, -4, -3, -2, -1]
+        
+        for _ in range(k):
+            result = heappop(negative)
+            
+        return -result
+
+    def find_kth_largest(self, nums: "list[int]", k: int) -> int:
+        # with sorting
+        nums.sort(reverse = True)
+        return nums[k - 1]
+
     def findKthLargest(self, nums: "list[int]", k: int) -> int:
-        pass
+        # neet code 
+        # quick select - kinda like quicksort
+        # average case o(n) - worst case o(n**2)
+        
+        # if the array is sorted
+        # kth largest element is:
+        k  = len(nums) - k
+
+        # left and right pointers
+        def quick_select(l, r):
+            # let pivot bve the rightmost element
+            # p is just left pointer
+            pivot, p = nums[r], l
+            # iterate over the list, except last element
+            for i in range(l, r):
+                # 
+                if nums[i] <= pivot:
+                    nums[p], nums[i] = nums[i], nums[p]
+                    # onto next position
+                    p += 1
+            # swapping the rightmost element with current 
+            # position of the pointer
+            nums[p], nums[r] = nums[r], nums[p]
+
+
+            if p > k :
+                # look at the left portion
+                return quick_select(l , p - 1)
+            elif p < k:
+                # look at the right portion
+                return quick_select(p + 1, r)
+            else: 
+                # we found the element
+                return nums[p]
+
+        return quick_select(0, len(nums) - 1)
