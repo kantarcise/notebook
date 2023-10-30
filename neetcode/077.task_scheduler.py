895a87d916903ecb0174418a43fba7cac2063800
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2023-10-31 00:03:32 +0300

    The Perfect Hour :stopwatch:

--- neetcode/077.task_scheduler.py
+++ neetcode/077.task_scheduler.py
@@ -51,8 +51,98 @@ Constraints:
 
 Takeaway:
 
+we have a tasks list
+we need to make sure that there is at least 
+n difference between same tasks
+        
+we need to know the most frequent task
+because it will occur first in our solution
+
+we will use a max heap
+whichch task is most frequent - time complexity o(log n)
+
+we will also use a queue to hold the frequencies of tasks
+and the time they will be scheduled again
+
 """
+
+from collections import Counter, deque
+import heapq
 
 class Solution:
     def findKthLargest(self, tasks: "list[int]", n: int) -> int:
-        pass
+        
+        # we have a tasks list
+        # we need to make sure that there is at least 
+        # n difference between same tasks
+        
+        # we need to know the most frequent task
+        # because it will occur first in our solution
+
+        # we will use a max heap
+        # whichch task is most frequent - time complexity o(log n)
+
+        # we will also use a queue to hold the frequencies of tasks
+        # and the time they will be scheduled again
+
+        # count the occurrences of each task
+        count = Counter(tasks)
+        
+        # Create a max heap to store negative task 
+        # counts (most frequent tasks first)
+        max_heap = [-cnt for cnt in count.values()]
+        heapq.heapify(max_heap)
+        
+        # Initialize time to keep track of the current time
+        time = 0
+        
+        # Create a queue to hold frequencies of 
+        # tasks and the time they will be scheduled again
+        q = deque()
+
+        # Main loop
+        while max_heap or q:
+            time += 1
+
+            if max_heap:
+                # Retrieve the most frequent task and 
+                # update its count
+                cnt = 1 + heapq.heappop(max_heap)
+                
+                # If the count is not zero, schedule it for later
+                if cnt:
+                    q.append([cnt, time + n])
+
+            if q and q[0][1] == time:
+                # Check if a task is scheduled to be 
+                # executed at the current time
+                heapq.heappush(max_heap, q.popleft()[0])
+
+        # Return the total time taken to complete 
+        # the tasks with intervals
+        return time
+
+
+    # much smaller approach
+    def leastInterval(self, tasks: "list[int]", n: int) -> int:
+        # Get the count of each task using Counter and
+        #  store them in a list
+        counts = list(Counter(tasks).values())
+        
+        # Find the maximum count, i.e., the most repeated task
+        most_repeats = max(counts)
+        
+        # Count how many tasks have the maximum count
+        num_longest = counts.count(most_repeats)
+        
+        # Calculate the minimum time required to 
+        # complete the tasks
+        # 
+        # This is based on the maximum count and the
+        # cooling interval (n)
+        # 
+        # If there are gaps between the most repeated 
+        # tasks, consider them
+        # 
+        # Otherwise, return the length of the tasks list
+        return max(len(tasks), (most_repeats-1) * (n+1) + num_longest)
