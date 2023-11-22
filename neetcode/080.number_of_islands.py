6632b007f16ac8cf0d6699816bb4468db562dbb3
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2023-11-22 23:37:17 +0300

    The Perfect Hour :stopwatch:

--- neetcode/080.number_of_islands.py
+++ neetcode/080.number_of_islands.py
@@ -38,8 +38,94 @@ grid[i][j] is '0' or '1'.
 
 Takeaway:
 
+To solve this question, think like a kindergardener.
 
+How can you find a single island?
+
+You look.
+
+For each 1, you need to check neighbours, this is clearly bfs or dfs.
+
+Setting the cell to 0 in order to not visit it again is pretty cool.
 """
+
+from collections import deque
+
 class Solution:
     def numIslands(self, grid: "list[list[str]]") -> int:
-        pass
+        # it is just bfs man. We got this.
+        if not grid:
+            return 0
+        
+        rows , cols = len(grid), len(grid[0])
+
+        visited = set()
+        islands = 0
+
+        def bfs(r ,c):
+            q = deque()
+            visited.add((r,c))
+            # add the coordinates to the queue
+            q.append((r,c))
+
+            while q:
+                row, col = q.popleft()
+                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
+                for deltar, deltac in directions:
+                    r , c = row + deltar , col + deltac
+
+                    if (r in range(rows) and
+                        c in range(cols) and
+                        grid[r][c] == "1" and
+                        (r, c) not in visited):
+                        # add the coordinate to queue
+                        q.append((r, c)) 
+                        visited.add((r,c))
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    # we found a new Island!
+                    # lets see how big it is?
+                    bfs(r,c)
+                    islands += 1
+
+        return islands
+    
+    def numIslands_dfs(self, grid: list[list[str]]) -> int:
+        
+        row = len(grid)
+        column = len(grid[0])
+        result = 0
+
+        def dfs(i, j):
+
+            # return if not proper island
+            if (i < 0 or 
+                j < 0 or 
+                i >= row or 
+                j >= column or 
+                grid[i][j] == '0'):
+                    return
+
+            #  Otherwise, it marks the current cell 
+            # as visited by setting it to '0' and recursively 
+            # calls dfs on its neighbors 
+            # (up, down, left, and right).
+            grid[i][j] = '0'
+
+            # 4 direction dfs. cool
+            dfs(i - 1, j)
+            dfs(i + 1, j)
+            dfs(i, j - 1)
+            dfs(i, j + 1)
+            
+        for i in range(row):
+            for j in range(column):
+                if grid[i][j] == '1':
+                    result += 1
+                    dfs(i, j)
+        
+        return result
+        
