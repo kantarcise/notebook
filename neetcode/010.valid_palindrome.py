88c8cd7f8818838927a4e359e50e20ff802025b2
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-03-27 14:18:23 +0300

    The Perfect Hour :stopwatch:

--- neetcode/010.valid_palindrome.py
+++ neetcode/010.valid_palindrome.py
@@ -1,21 +1,24 @@
 """
+A phrase is a palindrome if, after converting all uppercase letters 
+into lowercase letters and removing all non-alphanumeric 
+characters, it reads the same forward and backward. 
 
-A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+Alphanumeric characters include letters and numbers.
 
 Given a string s, return true if it is a palindrome, or false otherwise.
-
- 
 
 Example 1:
 
 Input: s = "A man, a plan, a canal: Panama"
 Output: true
 Explanation: "amanaplanacanalpanama" is a palindrome.
+
 Example 2:
 
 Input: s = "race a car"
 Output: false
 Explanation: "raceacar" is not a palindrome.
+
 Example 3:
 
 Input: s = " "
@@ -23,39 +26,37 @@ Output: true
 Explanation: s is an empty string "" after removing non-alphanumeric characters.
 Since an empty string reads the same forward and backward, it is a palindrome.
  
-
 Constraints:
 
-1 <= s.length <= 2 * 105
-s consists only of printable ASCII characters.
-
+  1 <= s.length <= 2 * 10^5
+  s consists only of printable ASCII characters.
 
 Takeaway:
 
-Palindrome simply means that reverse of the string is equal to original string
+  Palindrome simply means that reverse of the string is equal to original string
 
-strings have wonderful methods:
+  strings have wonderful methods:
 
-string.isalpha()        and         string.lower()
+  string.isalpha()        and         string.lower()
 
-str_seq[::-1] is reversed of str_seq
+  str_seq[::-1] is reversed of str_seq
 
-Other wonderful string methods:
+  Other wonderful string methods:
 
-string.strip() , string.split() , string.capitalize()
+  string.strip() , string.split() , string.capitalize()
 
-string.isdigit(), string.title(), string.starswith(), string.endswith()
+  string.isdigit(), string.title(), string.starswith(), string.endswith()
 
-You can use two pointers approach.
-In this example, this basically means to check some conditions for each of the 
-pointers and progress both of them.
+  You can use two pointers approach.
+  In this example, this basically means to check some conditions for each of the 
+  pointers and progress both of them.
 
 """
 
 class Solution:
-    # my first attempt
+   
     def is_palindrome_wrong_and_confused(self, s: str) -> bool:
-    
+        # my first attempt, does not work
         lower_s = "".join(s.lower())
 
         alphabetical_s = lower_s.maketrans("_=.*", "    ")
@@ -74,17 +75,40 @@ class Solution:
         return False
 
     def is_palindrome(self, s:str):
-        
+        # we can simply use string methods.
         new_str = ""
 
-        # basically for every alphabetic character, construct a new string
+        # basically for every alphabetic character, 
+        # construct a new string
         for c in s:
             if c.isalnum():
                 new_str += c.lower()
 
         # reverse a sequence
         return new_str == new_str[::-1]
+    
+    def isPalindrome_(self, s: str):
+        # we can use two pointers and not worry about 
+        # reversing the string
+        
+        l, r = 0 , len(s) - 1
 
+        while l < r:
+            # pass all non alphanumeric characters
+            while l < r and not s[l].isalpha():
+                l += 1
+            while r > l and not s[r].isalpha():
+                r -= 1
+
+            # check equality
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l + 1, r - 1
+
+        # if all passed, we found a Palindrome
+        return True
+
+ 
     def is_palindrome_two_pointers(self, s: str):
         """You can write the alphanumeric function yourself.
         Also you dont need to reverse the string. You can 
@@ -93,13 +117,18 @@ class Solution:
         l, r = 0 , len(s) - 1
 
         while l < r:
+            # pass all non alphanumeric characters
             while l < r and not self.alphanumeric(s[l]):
                 l += 1
             while r > l and not self.alphanumeric(s[r]):
                 r -= 1
+            
+            # check equality
             if s[l].lower() != s[r].lower():
                 return False
             l, r = l + 1, r - 1
+        
+        # if all passed, we found a Palindrome
         return True
         
     def alphanumeric(self, s):
