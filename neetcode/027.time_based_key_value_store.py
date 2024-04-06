e9ed21f4806d9b7886d0d9511471c72dbc58e54a
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-06 17:37:38 +0300

    The Perfect Hour :stopwatch:

--- neetcode/027.time_based_key_value_store.py
+++ neetcode/027.time_based_key_value_store.py
@@ -1,101 +1,73 @@
 """
-
 Design a time-based key-value data structure that can store multiple values
- for the same key at different time stamps and retrieve the key's value
+for the same key at different time stamps and retrieve the key's value
 at a certain timestamp.
 
 Implement the TimeMap class:
 
-TimeMap() Initializes the object of the data structure.
+    TimeMap() Initializes the object of the data structure.
 
-void set(String key, String value, int timestamp) Stores the key 'key' with the
- value 'value' at the given time timestamp.
+    void set(String key, String value, int timestamp) Stores the key 'key' 
+        with thevalue 'value' at the given time timestamp.
 
-String get(String key, int timestamp) Returns a value such that set was called
- previously, with timestamp_prev <= timestamp. If there are multiple such
-  values, it returns the value associated with the largest timestamp_prev.
-   If there are no values, it returns "".
+    String get(String key, int timestamp) Returns a value such that set was 
+        called previously, with timestamp_prev <= timestamp. If there are multiple such 
+        values, it returns the value associated with the largest timestamp_prev.
+        If there are no values, it returns "".
  
 Example 1:
 
-Input
+    Input
 
-["TimeMap", "set", "get", "get", "set", "get", "get"]
+        ["TimeMap", "set", "get", "get", "set", "get", "get"]
 
-[[], ["foo", "bar", 1], ["foo", 1], ["foo", 3], ["foo", "bar2", 4], ["foo", 4], ["foo", 5]]
+        [[], ["foo", "bar", 1], ["foo", 1], ["foo", 3], 
+                ["foo", "bar2", 4], ["foo", 4], ["foo", 5]]
 
-Output
+    Output
 
-[null, null, "bar", "bar", null, "bar2", "bar2"]
+        [null, null, "bar", "bar", null, "bar2", "bar2"]
 
-Explanation
+    Explanation
 
-TimeMap timeMap = new TimeMap();
+        TimeMap timeMap = new TimeMap();
 
-timeMap.set("foo", "bar", 1);  // store the key "foo" and value "bar" 
-along with timestamp = 1.
+        timeMap.set("foo", "bar", 1);  // store the key "foo" and value "bar" 
+        along with timestamp = 1.
 
-timeMap.get("foo", 1);         // return "bar"
+        timeMap.get("foo", 1);         // return "bar"
 
-timeMap.get("foo", 3);         // return "bar", since there is no value 
-corresponding to foo at timestamp 3 and timestamp 2, then the only value 
-is at timestamp 1 is "bar".
+        timeMap.get("foo", 3);         // return "bar", since there is no value 
+        corresponding to foo at timestamp 3 and timestamp 2, then the only value 
+        is at timestamp 1 is "bar".
 
-timeMap.set("foo", "bar2", 4); // store the key "foo" and value "bar2" along
- with timestamp = 4.
+        timeMap.set("foo", "bar2", 4); // store the key "foo" and value "bar2" along
+         with timestamp = 4.
 
-timeMap.get("foo", 4);         // return "bar2"
+        timeMap.get("foo", 4);         // return "bar2"
 
-timeMap.get("foo", 5);         // return "bar2"
+        timeMap.get("foo", 5);         // return "bar2"
  
 
 Constraints:
 
-1 <= key.length, value.length <= 100
-key and value consist of lowercase English letters and digits.
-1 <= timestamp <= 107
-All the timestamps timestamp of set are strictly increasing.
-At most 2 * 105 calls will be made to set and get.
+    1 <= key.length, value.length <= 100
+    key and value consist of lowercase English letters and digits.
+    1 <= timestamp <= 10^7
+    All the timestamps timestamp of set are strictly increasing.
+    At most 2 * 10^5 calls will be made to set and get.
 
 Takeaway:
 
-
+    Obviously we can use a dictionary.
 """
-
-# my first try - not complete - not true
-class TimeMap_:
-
-    def __init__(self):
-        # key is a string
-        # value is a list of value and timestamp
-        self.map = {}
-
-    def set(self, key: str, value: str, timestamp: int) -> None:
-        
-        if key in self.map:
-            self.map[key].append([value, timestamp])
-            return
-        else:
-            self.map[key] = []
-            self.map[key].append([value, timestamp])
-            return
-
-    def get(self, key: str, timestamp: int) -> str:
-        if key in self.map:
-            # return the highest timestamp value
-            result_list = self.map.get(key)
-            difference = map (lambda a : timestamp - a, [elem[1] for elem in result_list])
-            # find index of min difference
-            # return that element from the result_list
-        else:
-            return
- 
 
 class TimeMap:
     """The idea of yourself is not bad.
-    But for get method you can use the fact that timestamps are strictly increasing
-    If that was not the case, you would have to sort the list of timestamps for each get"""
-
+    But for get method you can use the fact that timestamps 
+    are strictly increasing.
+    If that was not the case, you would have to sort the list 
+    of timestamps for each get"""
 
     def __init__(self):
         # key is a string
@@ -113,7 +85,7 @@ class TimeMap:
 
         values = self.map.get(key, [])
 
-        #binary search
+        # binary search
         l, r = 0 , len(values) - 1
         while l <= r:
             m = l + ((r - l) // 2)
@@ -131,9 +103,3 @@ class TimeMap:
 # obj = TimeMap()
 # obj.set(key,value,timestamp)
 # param_2 = obj.get(key,timestamp)
-
-
-if __name__ == "__main__":
-    obj = TimeMap()
-    obj.set(key,value,timestamp)
-    param_2 = obj.get(key,timestamp)
