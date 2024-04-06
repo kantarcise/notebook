732d4b8b704a76517d3e6d2b3ce3e0db01e1c956
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-06 18:35:05 +0300

    The Perfect Hour :stopwatch:

--- neetcode/043.lru_cache.py
+++ neetcode/043.lru_cache.py
@@ -1,63 +1,72 @@
 """
-Design a data structure that follows the constraints
- of a Least Recently Used (LRU) cache.
+Design a data structure that follows the constraints 
+of a Least Recently Used (LRU) cache.
 
 Implement the LRUCache class:
 
-LRUCache(int capacity) Initialize the LRU cache with
- positive size capacity.
+    LRUCache(int capacity) Initialize the LRU cache with
+        positive size capacity.
 
-int get(int key) Return the value of the key if the
-key exists, otherwise return -1.
+    int get(int key) Return the value of the key if the 
+        key exists, otherwise return -1.
 
-void put(int key, int value) Update the value of
- the key if the key exists. Otherwise, add the
-key-value pair to the cache. If the number of keys
-exceeds the capacity from this operation, evict
- the least recently used key.
+    void put(int key, int value) Update the value of 
+        the key if the key exists. Otherwise, add the
+        key-value pair to the cache. If the number of keys 
+        exceeds the capacity from this operation, evict
+        the least recently used key.
 
 The functions get and put must each run in O(1) 
 average time complexity.
 
 Example 1:
 
-Input
-["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
-[[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+    Input
+    ["LRUCache", "put", "put", "get", "put", 
+            "get", "put", "get", "get", "get"]
+    [[2], [1, 1], [2, 2], [1], [3, 3], 
+            [2], [4, 4], [1], [3], [4]]
 
-Output
-[null, null, null, 1, null, -1, null, -1, 3, 4]
+    Output
+    
+        [null, null, null, 1, null, -1, null, -1, 3, 4]
 
-Explanation
-LRUCache lRUCache = new LRUCache(2);
-lRUCache.put(1, 1); // cache is {1=1}
-lRUCache.put(2, 2); // cache is {1=1, 2=2}
-lRUCache.get(1);    // return 1
-lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
-lRUCache.get(2);    // returns -1 (not found)
-lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
-lRUCache.get(1);    // return -1 (not found)
-lRUCache.get(3);    // return 3
-lRUCache.get(4);    // return 4
+    Explanation
+    
+        LRUCache lRUCache = new LRUCache(2);
+
+        lRUCache.put(1, 1); // cache is {1=1}
+        lRUCache.put(2, 2); // cache is {1=1, 2=2}
+        lRUCache.get(1);    // return 1
+        lRUCache.put(3, 3); // LRU key was 2, evicts key 2, 
+                                    cache is {1=1, 3=3}
+
+        lRUCache.get(2);    // returns -1 (not found)
+        lRUCache.put(4, 4); // LRU key was 1, evicts key 1, 
+                                    cache is {4=4, 3=3}
+
+        lRUCache.get(1);    // return -1 (not found)
+        lRUCache.get(3);    // return 3
+        lRUCache.get(4);    // return 4
  
 Constraints:
 
-1 <= capacity <= 3000
-0 <= key <= 104
-0 <= value <= 105
-At most 2 * 105 calls will be made to get and put.
+    1 <= capacity <= 3000
+    0 <= key <= 10^4
+    0 <= value <= 10^5
+    At most 2 * 10^5 calls will be made to get and put.
 
 Takeaway:
 
-We will have a capacity
+    We will have a capacity
 
-we will have a doubly linked list, also a hash map
-with keys and values as pointers to nodes.
+    we will have a doubly linked list, also a hash map
+    with keys and values as pointers to nodes.
 
-LRU and most recent will be pointed and updated.
-So we need two pointers just for those. Which will be Nodes as well.
+    LRU and most recent will be pointed and updated.
+    So we need two pointers just for those. Which will be Nodes as well.
 
-You can also use Queues, implemented with Python lists
+    You can also use Queues, implemented with Python lists
 
 """
 
@@ -150,6 +159,13 @@ class LRUCache:
             lru = self.left.next
             self.remove(lru)
             del self.cache[lru.key]
+
+
+"""
+Here is something extra
+
+Not ideal, queue with list. popping from beginning
+"""
 
 from collections import deque
 
