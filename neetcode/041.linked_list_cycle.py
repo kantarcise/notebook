34f8524abfe4d2141076a48443c05b382447189e
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-06 18:29:13 +0300

    The Perfect Hour :stopwatch:

--- neetcode/041.linked_list_cycle.py
+++ neetcode/041.linked_list_cycle.py
@@ -2,60 +2,76 @@
 Given head, the head of a linked list, determine if 
 the linked list has a cycle in it.
 
-There is a cycle in a linked list if there is some
- node in the list that can be reached again by continuously
-following the next pointer. Internally, pos is used to denote
-the index of the node that tail's next pointer is connected 
-to. Note that pos is not passed as a parameter.
+There is a cycle in a linked list if there is some node 
+in the list that can be reached again by continuously
+following the next pointer. 
 
-Return true if there is a cycle in the linked list. Otherwise, return false.
+Internally, pos is used to denote the index of the node 
+that tail's next pointer is connected 
+to. 
+
+Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. 
+
+Otherwise, return false.
 
 Example 1:
 
-Input: head = [3,2,0,-4], pos = 1
-Output: true
+    Input: head = [3,2,0,-4], pos = 1
+    Output: true
 
-Explanation: There is a cycle in the linked list, where the
- tail connects to the 1st node (0-indexed).
+    Explanation: 
+    
+        There is a cycle in the linked list, where the tail 
+        connects to the 1st node (0-indexed).
 
 Example 2:
 
-Input: head = [1,2], pos = 0
-Output: true
-Explanation: There is a cycle in the linked list, where the
-tail connects to the 0th node.
+    Input: head = [1,2], pos = 0
+    Output: true
+    
+    Explanation: 
+        
+        There is a cycle in the linked list, where the
+        tail connects to the 0th node.
 
 Example 3:
 
-Input: head = [1], pos = -1
-Output: false
-Explanation: There is no cycle in the linked list.
+    Input: head = [1], pos = -1
+    Output: false
+    
+    Explanation: 
+        
+        There is no cycle in the linked list.
  
 
 Constraints:
 
-The number of the nodes in the list is in the range [0, 104].
--105 <= Node.val <= 105
-pos is -1 or a valid index in the linked-list.
+    The number of the nodes in the list is in the range [0, 104].
+    -10^5 <= Node.val <= 10^5
+    pos is -1 or a valid index in the linked-list.
  
-Follow up: Can you solve it using O(1) (i.e. constant) memory?
+Follow up: 
+
+    Can you solve it using O(1) (i.e. constant) memory?
 
 Takeaway:
 
-We are trying to understand if the traversal of 
-the LL is an infinite loop
+    We are trying to understand if the traversal of 
+    the LL is an infinite loop
 
-If there is a cycle in it, it should be infinite
+    If there is a cycle in it, it should be infinite
 
-BUt the most precise way is to check whether we 
-pass through some arbitrary node, twice
+    BUt the most precise way is to check whether we 
+    pass through some arbitrary node, twice
 
-lets use slow and fast pointers. 
-Floyd's Tortoise and Hare
+    lets use slow and fast pointers. 
+    Floyd's Tortoise and Hare
 
-if fast pointer catches slow pointer, there has to be a cycle
+    if fast pointer catches slow pointer, there has to be a cycle
 
-Just a reminder, you can use a hashset for this question
+    Just a reminder, you can use a hashset for this question
 
 """
 
@@ -66,21 +82,6 @@ class ListNode:
         self.next = None
 
 class Solution:
-
-    # absolutely no correlation between the solution and this
-    def has_cycle(self, head):
-        
-        pointer = head 
-        length = 0 
-
-        while pointer:
-            length += 1
-            pointer = pointer.next
-
-        if length == 1:
-            return False
-
-        return True
 
     def hasCycle(self, head):
         # we are trying to understand if the traversal of 
