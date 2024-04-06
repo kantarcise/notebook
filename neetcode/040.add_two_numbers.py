4e830a5d2caaf80a247cc97a3857851163a82486
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-06 18:24:03 +0300

    The Perfect Hour :stopwatch:

--- neetcode/040.add_two_numbers.py
+++ neetcode/040.add_two_numbers.py
@@ -1,26 +1,30 @@
 """
-You are given two non-empty linked lists representing two non-negative integers. 
-The digits are stored in reverse order, and each of their nodes 
-contains a single digit. Add the two numbers and return 
-the sum as a linked list.
+You are given two non-empty linked lists representing two 
+non-negative integers. 
 
-You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+The digits are stored in reverse order, and each of their 
+nodes contains a single digit. 
+
+Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading 
+zero, except the number 0 itself.
 
 Example 1:
 
-Input: l1 = [2,4,3], l2 = [5,6,4]
-Output: [7,0,8]
-Explanation: 342 + 465 = 807.
+    Input: l1 = [2,4,3], l2 = [5,6,4]
+    Output: [7,0,8]
+    Explanation: 342 + 465 = 807.
 
 Example 2:
 
-Input: l1 = [0], l2 = [0]
-Output: [0]
+    Input: l1 = [0], l2 = [0]
+    Output: [0]
 
 Example 3:
 
-Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
-Output: [8,9,9,9,0,0,0,1]
+    Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+    Output: [8,9,9,9,0,0,0,1]
 
 Constraints:
 
@@ -30,23 +34,20 @@ Constraints:
 
 Takeaway:
 
-I was stuck in thinking the inequality of sizes for LL's. Turns out you can 
-just add 0 nodes to the one that is missing.
+    I was stuck in thinking the inequality of sizes for LL's. Turns out you can 
+    just add 0 nodes to the one that is missing.
 
-My approch would work, but its not why the question is asked.
+    My approch would work, but its not why the question is asked.
 
-Do not forget about trying to understand the question. Calm in the first 3 minutes.
+    Do not forget about trying to understand the question. Calm in the first 3 minutes.
 
-we check if the node is None for traversing the LinkedList.
+    we check if the node is None for traversing the LinkedList.
 
-In addition, we have a simple condition for the sum called "CARRY"
+    In addition, we have a simple condition for the sum called "CARRY"
 
 """
 
 # Definition for singly-linked list.
-from multiprocessing import dummy
-
-
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -54,8 +55,9 @@ class ListNode:
 
 class Solution:
 
-    # my first approach
-    def add_two_numbers(self, l1, l2):
+    def addTwoNumbers_(self, l1, l2):
+        # my first approach
+        
         # Input: l1 = [2,4,3], l2 = [5,6,4]
         # Output: [7,0,8]
         # Explanation: 342 + 465 = 807.
@@ -109,7 +111,10 @@ class Solution:
 
 if __name__ == '__main__':
 
-    sol = Solution()
-    print(sol.addTwoNumbers(l1 = [2,4,3], l2 = [5,6,4]))
-    print(sol.addTwoNumbers(l1 = [0], l2 = [0]))
-    print(sol.addTwoNumbers( l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]))
+    # cannot really make it work
+    
+    # sol = Solution()
+    # print(sol.addTwoNumbers(l1 = [2,4,3], l2 = [5,6,4]))
+    # print(sol.addTwoNumbers(l1 = [0], l2 = [0]))
+    # print(sol.addTwoNumbers( l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]))
+    pass
