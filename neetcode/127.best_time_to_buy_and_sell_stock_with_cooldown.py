223a22ee7f689b8e6d1f68038c20307fec9f99e0
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-14 00:02:30 +0300

    The Perfect Hour :stopwatch:

--- neetcode/127.best_time_to_buy_and_sell_stock_with_cooldown.py
+++ neetcode/127.best_time_to_buy_and_sell_stock_with_cooldown.py
@@ -2,43 +2,49 @@
 You are given an array prices where prices[i] is the 
 price of a given stock on the ith day.
 
-Find the maximum profit you can achieve. You may complete as many 
-transactions as you like (i.e., buy one and sell one share of 
-the stock multiple times) with the following restrictions:
+Find the maximum profit you can achieve. 
 
-After you sell your stock, you cannot buy stock on the 
-next day (i.e., cooldown one day).
+You may complete as many transactions as you like (i.e., buy 
+one and sell one share of the stock multiple times) with the 
+following restrictions:
+
+    After you sell your stock, you cannot buy stock on the 
+    next day (i.e., cooldown one day).
 
 Note: You may not engage in multiple transactions 
 simultaneously (i.e., you must sell the stock before you buy again).
 
 Example 1:
 
-Input: prices = [1,2,3,0,2]
-Output: 3
+    Input: prices = [1,2,3,0,2]
+    
+    Output: 3
 
-Explanation: transactions = [buy, sell, cooldown, buy, sell]
+    Explanation: transactions = [buy, sell, cooldown, buy, sell]
 
 Example 2:
 
-Input: prices = [1]
-Output: 0
+    Input: prices = [1]
+    
+    Output: 0
  
 Constraints:
 
     1 <= prices.length <= 5000
+    
     0 <= prices[i] <= 1000
 
 Takeaway:
 
-2D DP, simply a decision tree.
+    2D DP, simply a decision tree.
 
-Which is derived with basic thinking, as always.
+    Which is derived with basic thinking, as always.
 
-Caching is key!
-
+    Caching is key!
 """
+
 class Solution:
+
     def maxProfit(self, prices: list[int]) -> int:
         # we want to maximize the index i and index i + 1
         # to make a profit
