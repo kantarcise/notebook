3577d79f823126f9dd9792ca3abca5f468a7f268
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-13 23:45:39 +0300

    The Perfect Hour :stopwatch:

--- neetcode/117.partition_labels.py
+++ neetcode/117.partition_labels.py
@@ -1,6 +1,8 @@
 """
-You are given a string s. We want to partition the string 
-into as many parts as possible so that each letter appears 
+You are given a string s. 
+
+We want to partition the string into as many parts 
+as possible so that each letter appears 
 in at most one part.
 
 Note that the partition is done so that after concatenating 
@@ -8,36 +10,39 @@ all the parts in order, the resultant string should be s.
 
 Return a list of integers representing the size of these parts.
 
-
 Example 1:
 
-Input: s = "ababcbacadefegdehijhklij"
-Output: [9,7,8]
-Explanation:
-The partition is "ababcbaca", "defegde", "hijhklij".
-This is a partition so that each letter appears in at most one part.
+    Input: s = "ababcbacadefegdehijhklij"
+    
+    Output: [9,7,8]
+    
+    Explanation:
+    
+        The partition is "ababcbaca", "defegde", "hijhklij".
+        This is a partition so that each letter appears in at most one part.
 
-A partition like "ababcbacadefegde", "hijhklij" is incorrect, because 
-it splits s into less parts.
+        A partition like "ababcbacadefegde", "hijhklij" is incorrect, because 
+        it splits s into less parts.
 
 Example 2:
 
-Input: s = "eccbbbbdec"
-Output: [10]
+    Input: s = "eccbbbbdec"
+
+    Output: [10]
 
 Constraints:
 
     1 <= s.length <= 500
+    
     s consists of lowercase English letters.
 
 Takeaway:
 
-Partitions are CONTINUOUS.
+    Partitions are CONTINUOUS.
 
-If an element is in a partition, it has to be in it to win it.
+    If an element is in a partition, it has to be in it to win it.
 
-A dictionary for element - lastindex is cool.
-
+    A dictionary for element - lastindex is cool.
 """
 
 class Solution:
