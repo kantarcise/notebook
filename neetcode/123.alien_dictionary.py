ab40f2a3a7ef7b2f55c37edee23f410af2a792b7
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-13 23:55:28 +0300

    The Perfect Hour :stopwatch:

--- neetcode/123.alien_dictionary.py
+++ neetcode/123.alien_dictionary.py
@@ -7,9 +7,11 @@ You receive a list of non-empty strings words from
 the dictionary, where the words are sorted lexicographically 
 based on the rules of this new language.
 
-Derive the order of letters in this language. If the 
-order is invalid, return an empty string. If there 
-are multiple valid order of letters, return any of them.
+Derive the order of letters in this language. 
+
+If the order is invalid, return an empty string. 
+
+If there are multiple valid order of letters, return any of them.
 
 A string a is lexicographically smaller than a 
 string b if either of the following is true:
@@ -20,45 +22,49 @@ string b if either of the following is true:
 
 Example 1:
 
-Input: ["z","o"]
+    Input: ["z","o"]
 
-Output: "zo"
-Explanation:
-    From "z" and "o", we know 'z' < 'o', so return "zo".
+    Output: "zo"
+    
+    Explanation:
+        From "z" and "o", we know 'z' < 'o', so return "zo".
 
 Example 2:
 
-Input: ["hrn","hrf","er","enn","rfnn"]
+    Input: ["hrn","hrf","er","enn","rfnn"]
 
-Output: "hernf"
+    Output: "hernf"
 
-Explanation:
+    Explanation:
 
-    from "hrn" and "hrf", we know 'n' < 'f'
-    from "hrf" and "er", we know 'h' < 'e'
-    from "er" and "enn", we know get 'r' < 'n'
-    from "enn" and "rfnn" we know 'e'<'r'
-    so one possibile solution is "hernf"
+        from "hrn" and "hrf", we know 'n' < 'f'
+        from "hrf" and "er", we know 'h' < 'e'
+        from "er" and "enn", we know get 'r' < 'n'
+        from "enn" and "rfnn" we know 'e'<'r'
+        so one possibile solution is "hernf"
 
 Constraints:
 
-    The input words will contain characters only from lowercase 'a' to 'z'.
+    The input words will contain characters only from 
+        lowercase 'a' to 'z'.
+    
     1 <= words.length <= 100
+    
     1 <= words[i].length <= 100
 
 Takeaway:
 
-Hard one. We will need an adjaceny list.
+    Hard one. We will need an adjaceny list.
 
-The comparison is just like Python strings.
+    The comparison is just like Python strings.
 
-DFS is fancy.
-
+    DFS is fancy.
 """
 
 from collections import defaultdict
 
 class Solution:
+
     def foreignDictionary_(self, words: list[str]) -> str:
         # failed.
         # we can find set of rules for each duo
@@ -114,7 +120,7 @@ class Solution:
         return "".join(reversed(result))
 
     def foreignDictionary(self, words: list[str]) -> str:
-        # neet
+        # this works
 
         # this question will involve, topological sort
         # which means a Directed Acrilic Graph
