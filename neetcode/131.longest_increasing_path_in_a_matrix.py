3a8909deb40eb6a4bbeed53fd9fdc2f4d31f75e7
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-14 00:14:32 +0300

    The Perfect Hour :stopwatch:

--- neetcode/131.longest_increasing_path_in_a_matrix.py
+++ neetcode/131.longest_increasing_path_in_a_matrix.py
@@ -2,47 +2,54 @@
 Given an m x n integers matrix, return the length 
 of the longest increasing path in matrix.
 
-From each cell, you can either move in four 
-directions: left, right, up, or down. 
+From each cell, you can either move in four directions: 
+left, right, up, or down. 
 
 You may not move diagonally or move outside 
 the boundary (i.e., wrap-around is not allowed).
 
 Example 1:
 
-Input: matrix = [[9,9,4],[6,6,8],[2,1,1]]
-Output: 4
+    Input: matrix = [[9,9,4],[6,6,8],[2,1,1]]
+    
+    Output: 4
 
-Explanation: The longest increasing path is [1, 2, 6, 9].
+    Explanation: The longest increasing path is [1, 2, 6, 9].
 
 Example 2:
 
-Input: matrix = [[3,4,5],[3,2,6],[2,2,1]]
-Output: 4
+    Input: matrix = [[3,4,5],[3,2,6],[2,2,1]]
+    
+    Output: 4
 
-Explanation: The longest increasing path is [3, 4, 5, 6]. 
-Moving diagonally is not allowed.
+    Explanation: 
+    
+        The longest increasing path is [3, 4, 5, 6]. 
+        Moving diagonally is not allowed.
 
 Example 3:
 
-Input: matrix = [[1]]
-Output: 1
+    Input: matrix = [[1]]
+    
+    Output: 1
  
 Constraints:
 
     m == matrix.length
+    
     n == matrix[i].length
+    
     1 <= m, n <= 200
-    0 <= matrix[i][j] <= 231 - 1
+    
+    0 <= matrix[i][j] <= 2^31 - 1
 
 Takeaway:
 
-Memoization. Brute Force DFS.
+    Memoization. Brute Force DFS.
 
-Understand what if dfs returning, or why is it there?
+    Understand what if dfs returning, or why is it there?
 
-Call dfs from all cells - check all directions
-
+    Call dfs from all cells - check all directions
 """
 
 class Solution:
