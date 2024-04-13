bce9207ec5ec09b2fa6c37703d31c60939769763
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-13 22:02:31 +0300

    The Perfect Hour :stopwatch:

--- neetcode/075.k_closest_points_to_origin.py
+++ neetcode/075.k_closest_points_to_origin.py
@@ -1,61 +1,67 @@
 """
-Given an array of points where points[i] = [xi, yi] 
-represents a point on the X-Y plane and an integer k, return 
-the k closest points to the origin (0, 0).
+Given an array of points where points[i] = [xi, yi] represents 
+a point on the X-Y plane and an integer k, return the k 
+closest points to the origin (0, 0).
 
 The distance between two points on the X-Y plane is the Euclidean 
 distance (i.e., √(x1 - x2)2 + (y1 - y2)2).
 
-You may return the answer in any order. The answer is guaranteed 
-to be unique (except for the order that it is in).
+You may return the answer in any order. 
+
+The answer is guaranteed to be unique (except for the order that it is in).
 
 Example 1:
 
-Input: points = [[1,3],[-2,2]], k = 1
-Output: [[-2,2]]
+    Input: points = [[1,3],[-2,2]], k = 1
+    
+    Output: [[-2,2]]
 
-Explanation:
-The distance between (1, 3) and the origin is sqrt(10).
-The distance between (-2, 2) and the origin is sqrt(8).
-Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
+    Explanation:
+        
+        The distance between (1, 3) and the origin is sqrt(10).
+        The distance between (-2, 2) and the origin is sqrt(8).
+        Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
 
-We only want the closest k = 1 points from the origin, so 
-the answer is just [[-2,2]].
+        We only want the closest k = 1 points from the origin, so 
+            the answer is just [[-2,2]].
 
 Example 2:
 
-Input: points = [[3,3],[5,-1],[-2,4]], k = 2
-Output: [[3,3],[-2,4]]
+    Input: points = [[3,3],[5,-1],[-2,4]], k = 2
+    
+    Output: [[3,3],[-2,4]]
 
-Explanation: The answer [[-2,4],[3,3]] would also be accepted.
+    Explanation: 
+    
+        The answer [[-2,4],[3,3]] would also be accepted.
 
 Constraints:
 
-    1 <= k <= points.length <= 104
-    -104 <= xi, yi <= 104
+    1 <= k <= points.length <= 10^4
+    -10^4 <= xi, yi <= 10^4
 
 Takeaway:
 
-this solution works but it is O(n log n)
-[[1,3],[-2,2]], k = 1
-[[-2, 2]]
-        
-if we can calculate the euclidian distance for every point and
-sort the points list accordingly
-        
-we can return a slice of it.
+    this solution works but it is O(n log n)
+    [[1,3],[-2,2]], k = 1
+    [[-2, 2]]
 
-heap solution
+    if we can calculate the euclidian distance for every point and
+    sort the points list accordingly
 
-we only want k points, we do not have to 
-sort all of the list
-we can use a min heap
+    we can return a slice of it.
 
-we can calculate for [[1,3],[-2,2]]
-[10, 1, 3], [8, -2, 2]
-put them all in a min heap with heapify - which is o(n)
-and select k items, which would be 
-k log n which would be better than n log n
+    heap solution
+
+    we only want k points, we do not have to 
+    sort all of the list
+    we can use a min heap
+
+    we can calculate for [[1,3],[-2,2]]
+    [10, 1, 3], [8, -2, 2]
+    put them all in a min heap with heapify - which is o(n)
+    and select k items, which would be 
+    k log n which would be better than n log n
 
 """
 
@@ -64,7 +70,7 @@ from heapq import heapify, heappop, _heapify_max
 
 class Solution:
 
-    def kClosest(self, points: "list[list[int]]", k: int) -> "list[list[int]]":
+    def kClosest_(self, points: "list[list[int]]", k: int) -> "list[list[int]]":
         # this solution works but it is O(n log n)
         # [[1,3],[-2,2]], k = 1
         # [[-2, 2]]
@@ -88,7 +94,7 @@ class Solution:
 
         return [point[:2] for point in points_and_distance[:k]]
 
-    def k_closest(self, points: "list[list[int]]", k: int) -> "list[list[int]]":
+    def kClosest(self, points: "list[list[int]]", k: int) -> "list[list[int]]":
         # we only want k points, we do not have to 
         # sort all of the list
         # we can use a min heap
@@ -116,7 +122,7 @@ class Solution:
         return res
 
         # this is another way to go
-        return [[x, y] for _, x, y in [heappop(min_heap) for _ in range(k)]]
+        # return [[x, y] for _, x, y in [heappop(min_heap) for _ in range(k)]]
 
 
 if __name__ == '__main__':
