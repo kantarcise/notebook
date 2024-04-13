c170b6e25b4bf41bdbff9bc8872b2578472cbb88
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-13 23:04:28 +0300

    The Perfect Hour :stopwatch:

--- neetcode/108.meeting_rooms.py
+++ neetcode/108.meeting_rooms.py
@@ -1,49 +1,50 @@
 """
 Given an array of meeting time interval objects consisting 
 of start and end times 
-[[start_1,end_1],[start_2,end_2],...] (start_i < end_i), determine 
-if a person could add all meetings to their schedule 
+
+    [[start_1,end_1], [start_2,end_2],...] (start_i < end_i), 
+    
+determine if a person could add all meetings to their schedule 
 without any conflicts.
 
 Example 1:
 
-Input: intervals = [(0,30),(5,10),(15,20)]
+    Input: intervals = [(0,30),(5,10),(15,20)]
 
-Output: false
+    Output: false
 
-Explanation:
-(0,30),(5,10) and (0,30),(15,20) will conflict
+    Explanation:
+        
+        (0,30),(5,10) and (0,30),(15,20) will conflict
 
 Example 2:
 
-Input: intervals = [(5,8),(9,15)]
+    Input: intervals = [(5,8),(9,15)]
 
-Output: true
+    Output: true
+
+
 Note:
 
-(0,8),(8,10) is not considered a conflict at 8
+    (0,8),(8,10) is not considered a conflict at 8
+
 Constraints:
 
-0 <= intervals.length <= 100
-0 <= intervals[i].start < intervals[i].end <= 1000
+    0 <= intervals.length <= 100
+    
+    0 <= intervals[i].start < intervals[i].end <= 1000
 
 Takeaway:
 
-Simply sorting based on a key
+    Simply sorting based on a key
+    
+    And a single condition to check.
 
-And a single condition to check.
-
-"""
-
-"""
-Definition of Interval:
-class Interval(object):
-    def __init__(self, start, end):
-        self.start = start
-        self.end = end
 """
 
 class Interval(object):
+    # Definition of Interval:
+ 
     def __init__(self, start, end):
         self.start = start
         self.end = end
