992784efc3369e94448d9e1930db2bff7f612f95
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-13 20:06:39 +0300

    The Perfect Hour :stopwatch:

--- neetcode/044.merge_k_sorted_lists.py
+++ neetcode/044.merge_k_sorted_lists.py
@@ -37,7 +37,7 @@ Constraints:
     k == lists.length
     0 <= k <= 104
     0 <= lists[i].length <= 500
-    -104 <= lists[i][j] <= 104
+    -10^4 <= lists[i][j] <= 10^4
     lists[i] is sorted in ascending order.
     The sum of lists[i].length will not exceed 104.
 
@@ -117,6 +117,8 @@ class Solution:
         return lists[0]
 
     def mergeList(self, l1, l2):
+        # helper method
+        
         dummy = ListNode()
         tail = dummy
 
