f60f892051db947b29cc3cac2bd3faa3c3038e62
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-13 22:52:35 +0300

    The Perfect Hour :stopwatch:

--- neetcode/100.coin_change.py
+++ neetcode/100.coin_change.py
@@ -4,41 +4,47 @@ of different denominations and an integer amount representing
 a total amount of money.
 
 Return the fewest number of coins that you need to make 
-up that amount. If that amount of money cannot be made 
-up by any combination of the coins, return -1.
+up that amount. 
+
+If that amount of money cannot be made up by any combination 
+of the coins, return -1.
 
 You may assume that you have an infinite number of each 
 kind of coin.
 
 Example 1:
 
-Input: coins = [1,2,5], amount = 11
-Output: 3
-Explanation: 11 = 5 + 5 + 1
+    Input: coins = [1,2,5], amount = 11
+    
+    Output: 3
+    
+    Explanation: 11 = 5 + 5 + 1
 
 Example 2:
 
-Input: coins = [2], amount = 3
-Output: -1
+    Input: coins = [2], amount = 3
+    
+    Output: -1
 
 Example 3:
 
-Input: coins = [1], amount = 0
-Output: 0
+    Input: coins = [1], amount = 0
+    
+    Output: 0
 
 Constraints:
 
-1 <= coins.length <= 12
-1 <= coins[i] <= 231 - 1
-0 <= amount <= 104
+    1 <= coins.length <= 12
+    1 <= coins[i] <= 2^31 - 1
+    0 <= amount <= 10^4
 
 Takeaway:
 
-Bottom Up DP solution.
+    Bottom Up DP solution.
 
-step by step, get to the solution, using memoization
+    step by step, get to the solution, using memoization
 
-Understanding the question is key, as always.
+    Understanding the question is key, as always.
 """
 
 class Solution:
@@ -72,7 +78,7 @@ class Solution:
     
     def coinChange_(self, coins: list[int], amount: int) -> int:
         # incredible
-        # i dont even know
+        # yeah, I cannot even
         coins = list(filter(lambda coin: coin <= amount, coins))
         step = 0
         seen = 1<<amount
