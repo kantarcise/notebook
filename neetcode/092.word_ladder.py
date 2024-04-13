92cddcd4a305a5f76f0edb8e803a8b59a79e61af
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-13 22:39:50 +0300

    The Perfect Hour :stopwatch:

--- neetcode/092.word_ladder.py
+++ neetcode/092.word_ladder.py
@@ -1,52 +1,67 @@
 """
-A transformation sequence from word beginWord to word 
-endWord using a dictionary wordList is a sequence of words 
+A transformation sequence from word beginWord to word endWord using 
+a dictionary wordList is a sequence of words 
 beginWord -> s1 -> s2 -> ... -> sk such that:
 
-Every adjacent pair of words differs by a single letter.
+    Every adjacent pair of words differs by a single letter.
 
-Every si for 1 <= i <= k is in wordList. Note that 
-beginWord does not need to be in wordList.
+    Every si for 1 <= i <= k is in wordList. 
+        Note that beginWord does not need to be in wordList.
+    
     sk == endWord
 
-Given two words, beginWord and endWord, and a dictionary 
-wordList, return the number of words in the shortest 
-transformation sequence from beginWord to endWord, or 
-0 if no such sequence exists.
-
+Given two words, beginWord and endWord, and a dictionary wordList, return 
+the number of words in the shortest transformation sequence from 
+beginWord to endWord, or 0 if no such sequence exists.
 
 Example 1:
 
-Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
-Output: 5
+    Input: 
+        beginWord = "hit", endWord = "cog", 
+        wordList = ["hot","dot","dog","lot","log","cog"]
+    
+    Output: 5
 
-Explanation: One shortest transformation sequence is "hit" -> "hot" -> "dot" -> "dog" -> cog", which is 5 words long.
+    Explanation: 
+    
+    One shortest transformation sequence 
+        is "hit" -> "hot" -> "dot" -> "dog" -> cog", which is 5 words long.
 
 Example 2:
 
-Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
-Output: 0
+    Input: 
+        beginWord = "hit", endWord = "cog", 
+        wordList = ["hot","dot","dog","lot","log"]
+    
+    Output: 0
 
-Explanation: The endWord "cog" is not in wordList, therefore there is no valid transformation sequence.
+    Explanation: 
+        The endWord "cog" is not in wordList, therefore 
+            there is no valid transformation sequence.
 
 Constraints:
 
-1 <= beginWord.length <= 10
-endWord.length == beginWord.length
-1 <= wordList.length <= 5000
-wordList[i].length == beginWord.length
-beginWord, endWord, and wordList[i] consist of lowercase English letters.
-beginWord != endWord
-All the words in wordList are unique.
+    1 <= beginWord.length <= 10
+    
+    endWord.length == beginWord.length
+    
+    1 <= wordList.length <= 5000
+    
+    wordList[i].length == beginWord.length
+    
+    beginWord, endWord, and wordList[i] consist of lowercase English letters.
+    
+    beginWord != endWord
+    
+    All the words in wordList are unique.
 
 Takeaway:
 
-BFS - A SET AND A DEQUE IS A CLASSIC
+    BFS - A SET AND A DEQUE IS A CLASSIC
 
-Make a adjacency map with a defaultdict
+    Make a adjacency map with a defaultdict
 
-For shortest path, BFS is GOOD
-
+    For shortest path, BFS is GOOD
 """
 
 from collections import deque
@@ -88,7 +103,7 @@ class Solution:
         return True if len(Counter(word1) - Counter(word2)) == 1 else False
     
     def ladderLength__(self, beginWord: str, endWord: str, wordList: list[str]) -> int:
-        # homie
+        # expert help, works
         if endWord not in wordList:
             return 0
 
@@ -113,8 +128,6 @@ class Solution:
         return 0  # No transformation sequence found
     
     def ladderLength(self, beginWord: str, endWord: str, wordList: list[str]) -> int:
-        # neet
-        
         # we need to make the graph ourselves,
         # beginning word might not be in wordList
         
@@ -171,4 +184,3 @@ class Solution:
             
         # could not reach
         return 0
-
