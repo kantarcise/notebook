21c28a514db4a8abf7d7306cbe436c9efb8fd5e4
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-13 18:55:51 +0300

    The Perfect Hour :stopwatch:

--- neetcode/054.binary_tree_right_side_view.py
+++ neetcode/054.binary_tree_right_side_view.py
@@ -5,44 +5,44 @@ you can see ordered from top to bottom.
 
 Example 1:
 
-Input: root = [1,2,3,null,5,null,4]
-Output: [1,3,4]
+    Input: root = [1,2,3,null,5,null,4]
+    Output: [1,3,4]
 
 Example 2:
 
-Input: root = [1,null,3]
-Output: [1,3]
+    Input: root = [1,null,3]
+    Output: [1,3]
 
 Example 3:
 
-Input: root = []
-Output: []
+    Input: root = []
+    Output: []
 
 Constraints:
 
-The number of nodes in the tree is in the range [0, 100].
--100 <= Node.val <= 100
+    The number of nodes in the tree is in the range [0, 100].
+    -100 <= Node.val <= 100
 
 Takeaway:
 
-Only going right won't work
+    Only going right won't work
 
-The nodes on the left subtree can still have some right nodes
-that should have been taken into account
+    The nodes on the left subtree can still have some right nodes
+    that should have been taken into account
 
-We can use Breadth First Search - Level Ordering Traversal
+    We can use Breadth First Search - Level Ordering Traversal
 
-At each level, we will be searching for the right most node
+    At each level, we will be searching for the right most node
 
-Loop through all the nodes at the current level. 
-For each node, remove it from the left end of 
-the deque (q) using q.popleft().
+    Loop through all the nodes at the current level. 
+    For each node, remove it from the left end of 
+    the deque (q) using q.popleft().
 
-If the node is not None, update right_side to 
-the current node. This is because you are traversing 
-from left to right within the level, so the rightmost 
-node encountered will be the last one 
-assigned to right_side.
+    If the node is not None, update right_side to 
+    the current node. This is because you are traversing 
+    from left to right within the level, so the rightmost 
+    node encountered will be the last one 
+    assigned to right_side.
 """
 
 from collections import deque
@@ -56,7 +56,7 @@ class TreeNode:
 
 class Solution:
 
-    def right_side_view(self, root: "TreeNode") -> "list[int]":
+    def rightSideView_(self, root: "TreeNode") -> "list[int]":
         # first approach
         # starting from the node, only go to the right nodes.
         # return a list of those node values
@@ -78,9 +78,9 @@ class Solution:
     def rightSideView(self, root: "TreeNode") -> "list[int]":
         
         # lets use BFS
-        
         result = []
 
+        # we bfs we use a deque
         q = deque()
         q.append(root)
 
