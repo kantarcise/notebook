bdd785ec8982c9b448865a3f255c2fb59ab3d2f6
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-14 00:45:08 +0300

    The Perfect Hour :stopwatch:

--- neetcode/147.plus_one.py
+++ neetcode/147.plus_one.py
@@ -1,8 +1,10 @@
 """
-You are given a large integer represented as an integer 
-array digits, where each digits[i] is the ith digit of 
-the integer. The digits are ordered from most 
+You are given a large integer represented as an integer array 
+digits, where each digits[i] is the ith digit of the integer. 
+
+The digits are ordered from most 
 significant to least significant in left-to-right order. 
+
 The large integer does not contain any leading 0's.
 
 Increment the large integer by one and return the resulting 
@@ -10,43 +12,55 @@ array of digits.
 
 Example 1:
 
-Input: digits = [1,2,3]
-Output: [1,2,4]
-Explanation: The array represents the integer 123.
-Incrementing by one gives 123 + 1 = 124.
-Thus, the result should be [1,2,4].
+    Input: digits = [1,2,3]
+    
+    Output: [1,2,4]
+    
+    Explanation: 
+        
+        The array represents the integer 123.
+        Incrementing by one gives 123 + 1 = 124.
+        Thus, the result should be [1,2,4].
 
 Example 2:
 
-Input: digits = [4,3,2,1]
-Output: [4,3,2,2]
-Explanation: The array represents the integer 4321.
-Incrementing by one gives 4321 + 1 = 4322.
-Thus, the result should be [4,3,2,2].
+    Input: digits = [4,3,2,1]
+    
+    Output: [4,3,2,2]
+    
+    Explanation: 
+        
+        The array represents the integer 4321.
+        Incrementing by one gives 4321 + 1 = 4322.
+        Thus, the result should be [4,3,2,2].
 
 Example 3:
 
-Input: digits = [9]
-Output: [1,0]
-Explanation: The array represents the integer 9.
-Incrementing by one gives 9 + 1 = 10.
-Thus, the result should be [1,0].
+    Input: digits = [9]
+    
+    Output: [1,0]
+    
+    Explanation: 
+        
+        The array represents the integer 9.
+        Incrementing by one gives 9 + 1 = 10.
+        Thus, the result should be [1,0].
 
 Constraints:
 
-1 <= digits.length <= 100
-0 <= digits[i] <= 9
-digits does not contain any leading 0's.
+    1 <= digits.length <= 100
+    0 <= digits[i] <= 9
+    digits does not contain any leading 0's.
 
 Takeaway:
 
-map. List comprehension. you name it.
+    map. List comprehension. you name it.
 
-Explicit type conversion is also known as typecasting. 
+    Explicit type conversion is also known as typecasting. 
 
-str(), int(), float(), list()
-
+    str(), int(), float(), list()
 """
+
 class Solution:
     def plusOne_(self, digits: list[int]) -> list[int]:
         # weird flex but okay
