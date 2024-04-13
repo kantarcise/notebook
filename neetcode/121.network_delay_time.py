415d1853b56c2a72c6e082d456a015ccc69b8291
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-13 23:52:22 +0300

    The Perfect Hour :stopwatch:

--- neetcode/121.network_delay_time.py
+++ neetcode/121.network_delay_time.py
@@ -1,49 +1,59 @@
 """
 You are given a network of n nodes, labeled from 1 to n. 
+
 You are also given times, a list of travel times as 
 directed edges times[i] = (ui, vi, wi), where ui is the 
 source node, vi is the target node, and wi is the time it 
 takes for a signal to travel from source to target.
 
-We will send a signal from a given node k. Return the 
-minimum time it takes for all the n nodes to receive 
-the signal. If it is impossible for all the n nodes 
-to receive the signal, return -1.
+We will send a signal from a given node k. 
+
+Return the minimum time it takes for all the n nodes to receive 
+the signal. 
+
+If it is impossible for all the n nodes to receive the signal, return -1.
 
 Example 1:
 
-Input: times = [[2,1,1],[2,3,1],[3,4,1]], n = 4, k = 2
-Output: 2
+    Input: times = [[2,1,1],[2,3,1],[3,4,1]], n = 4, k = 2
+    
+    Output: 2
 
 Example 2:
 
-Input: times = [[1,2,1]], n = 2, k = 1
-Output: 1
+    Input: times = [[1,2,1]], n = 2, k = 1
+    
+    Output: 1
 
 Example 3:
 
-Input: times = [[1,2,1]], n = 2, k = 2
-Output: -1
- 
+    Input: times = [[1,2,1]], n = 2, k = 2
+    
+    Output: -1
 
 Constraints:
 
     1 <= k <= n <= 100
+    
     1 <= times.length <= 6000
+    
     times[i].length == 3
+    
     1 <= ui, vi <= n
+    
     ui != vi
+    
     0 <= wi <= 100
+    
     All the pairs (ui, vi) are unique. (i.e., no multiple edges.)
 
 Takeaway:
 
-Djikstra! - shortest path
+    Djikstra! - shortest path
 
-used with a set and minimum heap
+    used with a set and minimum heap
 
-See the code!
-
+    See the code!
 """
 
 from collections import defaultdict
@@ -144,8 +154,4 @@ class Solution:
                     # add the new path as added
                     heappush(min_heap, (w1 + w2, n2))
         
-        return t if len(visit) == n else -1 
-            
-        
-        
-        
+        return t if len(visit) == n else -1
