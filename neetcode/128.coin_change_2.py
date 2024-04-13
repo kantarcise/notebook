751b554642468c1cc62b4dc3a8157d93e7a159ce
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-14 00:03:33 +0300

    The Perfect Hour :stopwatch:

--- neetcode/128.coin_change_2.py
+++ neetcode/128.coin_change_2.py
@@ -3,7 +3,8 @@ You are given an integer array coins representing coins of
 different denominations and an integer amount representing a 
 total amount of money.
 
-Return the number of combinations that make up that amount. 
+Return the number of combinations that make up that amount.
+
 If that amount of money cannot be made up by any 
 combination of the coins, return 0.
 
@@ -15,52 +16,56 @@ signed 32-bit integer.
 
 Example 1:
 
-Input: amount = 5, coins = [1,2,5]
+    Input: amount = 5, coins = [1,2,5]
 
-Output: 4
+    Output: 4
 
-Explanation: there are four ways to make up the amount:
+    Explanation: there are four ways to make up the amount:
 
-    5=5
-    5=2+2+1
-    5=2+1+1+1
-    5=1+1+1+1+1
+        5=5
+        5=2+2+1
+        5=2+1+1+1
+        5=1+1+1+1+1
 
 Example 2:
 
-Input: amount = 3, coins = [2]
+    Input: amount = 3, coins = [2]
 
-Output: 0
+    Output: 0
 
-Explanation: the amount of 3 cannot be made up just with coins of 2.
+    Explanation: the amount of 3 cannot be made up just with coins of 2.
 
 Example 3:
 
-Input: amount = 10, coins = [10]
-Output: 1
+    Input: amount = 10, coins = [10]
+    
+    Output: 1
  
 Constraints:
 
     1 <= coins.length <= 300
+    
     1 <= coins[i] <= 5000
+    
     All the values of coins are unique.
+    
     0 <= amount <= 5000
 
 Takeaway:
 
-We can use a decision tree with memoization
+    We can use a decision tree with memoization
 
-we gotta think of repeating solutions!
+    we gotta think of repeating solutions!
 
-Or
+    Or
 
-We can use 2D DP solution, using the example
-
+    We can use 2D DP solution, using the example
 """
 
 class Solution:
+
     def change_(self, amount: int, coins: list[int]) -> int:
-        # neet
+        # works
         # memoization
         
         # based on a decision tree
@@ -114,8 +119,7 @@ class Solution:
         return dfs(0, 0)
     
     def change(self, amount: int, coins: list[int]) -> int:
-        # bottom up DP
-        
+        # works, bottom up DP
         
         #              amount
         #           5 4 3 2 1 0 
