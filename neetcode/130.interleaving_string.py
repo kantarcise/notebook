1ac946b7e6466ba2c9dde5843f6edc942c75260f
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-14 00:12:52 +0300

    The Perfect Hour :stopwatch:

--- neetcode/130.interleaving_string.py
+++ neetcode/130.interleaving_string.py
@@ -3,12 +3,11 @@ Given strings s1, s2, and s3, find whether s3 is formed
 by an interleaving of s1 and s2.
 
 An interleaving of two strings s and t is a configuration 
-where s and t are divided into n and m 
-substrings respectively, such that:
+where s and t are divided into n and m substrings respectively, such that:
 
-s = s1 + s2 + ... + sn
-t = t1 + t2 + ... + tm
-|n - m| <= 1
+    s = s1 + s2 + ... + sn
+    t = t1 + t2 + ... + tm
+    |n - m| <= 1
 
 The interleaving is s1 + t1 + s2 + t2 + s3 + t3 + ... 
                     
@@ -18,46 +17,50 @@ Note: a + b is the concatenation of strings a and b.
 
 Example 1:
 
-Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
-Output: true
-
-Explanation: One way to obtain s3 is:
-    Split s1 into s1 = "aa" + "bc" + "c", and s2 into s2 = "dbbc" + "a".
+    Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
     
-    Interleaving the two splits, we 
-            get "aa" + "dbbc" + "bc" + "a" + "c" = "aadbbcbcac".
-    
-    Since s3 can be obtained by interleaving s1 and s2, we return true.
+    Output: true
 
+    Explanation: One way to obtain s3 is:
+        Split s1 into s1 = "aa" + "bc" + "c", and s2 into s2 = "dbbc" + "a".
+
+        Interleaving the two splits, we 
+                get "aa" + "dbbc" + "bc" + "a" + "c" = "aadbbcbcac".
+
+        Since s3 can be obtained by interleaving s1 and s2, we return true.
 
 Example 2:
 
-Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
-Output: false
+    Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+    
+    Output: false
 
-Explanation: Notice how it is impossible to interleave 
-    s2 with any other string to obtain s3.
+    Explanation: Notice how it is impossible to interleave 
+        s2 with any other string to obtain s3.
 
 Example 3:
 
-Input: s1 = "", s2 = "", s3 = ""
-Output: true
+    Input: s1 = "", s2 = "", s3 = ""
+    
+    Output: true
  
 Constraints:
 
     0 <= s1.length, s2.length <= 100
+    
     0 <= s3.length <= 200
+    
     s1, s2, and s3 consist of lowercase English letters.
 
 Takeaway:
 
-Both memoization and bottom up will work.
-
-Simply understand the decision tree, which is based on
-indexes of two strings, to try to result in s3. 
-
-We are usually making out of bounds within the grid,
-just to have a simpler calculation.
+    Both memoization and bottom up will work.
+    
+    Simply understand the decision tree, which is based on
+    indexes of two strings, to try to result in s3. 
+    
+    We are usually making out of bounds within the grid,
+    just to have a simpler calculation.
 
 """
 class Solution:
