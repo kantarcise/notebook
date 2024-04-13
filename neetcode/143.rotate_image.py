2006ac9e30f2fc0f033f16e95d9f6506dbe13d38
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-14 00:38:23 +0300

    The Perfect Hour :stopwatch:

--- neetcode/143.rotate_image.py
+++ neetcode/143.rotate_image.py
@@ -3,34 +3,39 @@ You are given an n x n 2D matrix representing an image, rotate
 the image by 90 degrees (clockwise).
 
 You have to rotate the image in-place, which means you have to 
-modify the input 2D matrix directly. DO NOT allocate another 
-2D matrix and do the rotation.
+modify the input 2D matrix directly. 
+
+DO NOT allocate another 2D matrix and do the rotation.
 
 Example 1:
 
-Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
-Output: [[7,4,1],[8,5,2],[9,6,3]]
+    Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+    
+    Output: [[7,4,1],[8,5,2],[9,6,3]]
 
 Example 2:
 
-Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
-Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+    Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+    
+    Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
  
 Constraints:
 
-n == matrix.length == matrix[i].length
-1 <= n <= 20
--1000 <= matrix[i][j] <= 1000
+    n == matrix.length == matrix[i].length
+    
+    1 <= n <= 20
+    
+    -1000 <= matrix[i][j] <= 1000
 
 Takeaway:
 
-We got a really smart solution, and a simple one.
+    We got a really smart solution, and a simple one.
 
-Understanding both is cool.
-
+    Understanding both is cool.
 """
 
 class Solution:
+    
     def rotate_(self, matrix: list[list[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
@@ -94,7 +99,6 @@ class Solution:
             # move pointers so that you can go in for inner matrix
             r -= 1
             l += 1
-            
                 
     def rotate(self, matrix: list[list[int]]) -> None:
         """Instead of the solution above, you can do two operations on the matrix
@@ -117,4 +121,3 @@ class Solution:
         
         transpose(matrix)
         reflect(matrix)
-        
