bda769223fd7dd8876d5af6a9a844feece0a6e2b
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-13 22:59:13 +0300

    The Perfect Hour :stopwatch:

--- neetcode/105.insert_interval.py
+++ neetcode/105.insert_interval.py
@@ -1,51 +1,58 @@
 """
 You are given an array of non-overlapping intervals 
-intervals where intervals[i] = [start_i, end_i] represent 
-the start and the end of the ith interval and intervals is 
-sorted in ascending order by start_i. You are also given an 
-interval newInterval = [start, end] that represents the start 
-and end of another interval.
+`intervals` where intervals[i] = [start_i, end_i] represent the start 
+and the end of the ith interval and intervals is 
+sorted in ascending order by start_i. 
 
-Insert newInterval into intervals such that intervals is 
-still sorted in ascending order by start_i and intervals 
-still does not have any overlapping intervals (merge 
-overlapping intervals if necessary).
+You are also given an interval newInterval = [start, end] that represents 
+the start and end of another interval.
+
+Insert `newInterval` into `intervals` such that `intervals` is 
+still sorted in ascending order by start_i and intervals still does not 
+have any overlapping intervals (merge overlapping intervals if necessary).
 
 Return intervals after the insertion.
 
 Example 1:
 
-Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
-Output: [[1,5],[6,9]]
+    Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+    
+    Output: [[1,5],[6,9]]
 
 Example 2:
 
-Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], 
-        newInterval = [4,8]
-Output: [[1,2],[3,10],[12,16]]
+    Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], 
+            newInterval = [4,8]
+    Output: [[1,2],[3,10],[12,16]]
 
-Explanation: Because the new interval [4,8] overlaps 
-with [3,5],[6,7],[8,10].
+    Explanation: 
+    
+        Because the new interval [4,8] overlaps 
+            with [3,5],[6,7],[8,10].
 
 Constraints:
 
-0 <= intervals.length <= 104
-intervals[i].length == 2
-0 <= starti <= endi <= 105
-intervals is sorted by starti in ascending order.
-newInterval.length == 2
-0 <= start <= end <= 105
+    0 <= intervals.length <= 10^4
 
+    intervals[i].length == 2
+
+    0 <= starti <= endi <= 10^5
+
+    intervals is sorted by starti in ascending order.
+    
+    newInterval.length == 2
+    
+    0 <= start <= end <= 10^5
 
 Takeaway:
 
-Sorting is a great tool in interval questions.
+    Sorting is a great tool in interval questions.
 
-We gotta think of all edge cases.
-
+    We gotta think of all edge cases.
 """
 
 class Solution:
+
     def insert(self, intervals: list[list[int]], newInterval: list[int]) -> list[list[int]]:
         
         # intervals = [[1,3],[6,9]], newInterval = [2,5]
