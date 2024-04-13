94626683d24b5649cb497ebad1cb8e3da70abe90
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-13 21:53:11 +0300

    The Perfect Hour :stopwatch:

--- neetcode/071.letter_combinations_of_a_phone_number.py
+++ neetcode/071.letter_combinations_of_a_phone_number.py
@@ -1,48 +1,56 @@
 """
-Given a string containing digits from 2-9 inclusive, return all 
-possible letter combinations that the number could represent. 
+Given a string containing digits from 2-9 inclusive, return 
+all possible letter combinations that the number could represent. 
+
 Return the answer in any order.
 
-A mapping of digits to letters (just like on the 
-telephone buttons) is given below. Note that 1 
-does not map to any letters.
+A mapping of digits to letters (just like on the telephone buttons) is 
+given below. 
+
+Note that 1 does not map to any letters.
 
 Example 1:
 
-Input: digits = "23"
-Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+    Input: digits = "23"
+    
+    Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
 
 Example 2:
 
-Input: digits = ""
-Output: []
+    Input: digits = ""
+    
+    Output: []
 
 Example 3:
 
-Input: digits = "2"
-Output: ["a","b","c"]
+    Input: digits = "2"
+    
+    Output: ["a","b","c"]
 
 Constraints:
 
     0 <= digits.length <= 4
+    
     digits[i] is a digit in the range ['2', '9'].
 
 Takeaway:
 
-If we are deciding on a tree, consider backtracking
+    If we are deciding on a tree, consider backtracking
 
-Backtracking is generallt recursive. The base case is the end
-recurring is the next step on backtracking and updated path.
+    Backtracking is generally recursive. The base case is the end
+        recurring is the next step on backtracking and updated path.
 
-Mapping is clear - hashmap - dicts.
+    Mapping is clear - hashmap - dicts.
 """
 
 class Solution_:
     def letterCombinations(self, digits):
-        # first solution
+        # first solution, works and fast too
         ret = []
+
         if not digits:
             return ret
+        
         # map the phone
         # we will be given a string of digits
         phone = {"2":"abc", '3':"def", '4':"ghi", 
@@ -59,9 +67,9 @@ class Solution_:
         for c in m[digits[0]]:
             self.dfs(m, digits[1:], path+c, ret)
 
-
-class Solution:
-    def letterCombinations(self, digits):
+    def letterCombinations_(self, digits):
+        # this works too
+        
         # solution has to be brute force - backtracking
         # we need some data structure to map the digits
         # at each selection, we decide the choice of the digit we have
