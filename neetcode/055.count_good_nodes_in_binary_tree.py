8a198e30a7132f830d1c3afa35e1fd5e2e62e1c6
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-13 19:10:52 +0300

    The Perfect Hour :stopwatch:

--- neetcode/055.count_good_nodes_in_binary_tree.py
+++ neetcode/055.count_good_nodes_in_binary_tree.py
@@ -7,44 +7,45 @@ Return the number of good nodes in the binary tree.
 
 Example 1:
 
-Input: root = [3,1,4,3,null,1,5]
-Output: 4
+    Input: root = [3,1,4,3,null,1,5]
+    Output: 4
 
-Explanation: Nodes in blue are good.
+    Explanation: Nodes in blue are good.
 
-Root Node (3) is always a good node.
-Node 4 -> (3,4) is the maximum value in the path starting 
-from the root.
-Node 5 -> (3,4,5) is the maximum value in the path
-Node 3 -> (3,1,3) is the maximum value in the path.
+        Root Node (3) is always a good node.
+        Node 4 -> (3,4) is the maximum value in the path starting 
+            from the root.
+        Node 5 -> (3,4,5) is the maximum value in the path
+        Node 3 -> (3,1,3) is the maximum value in the path.
 
 Example 2:
 
-Input: root = [3,3,null,4,2]
-Output: 3
-Explanation: Node 2 -> (3, 3, 2) is not good, because "3" 
-is higher than it.
+    Input: root = [3,3,null,4,2]
+    Output: 3
+    
+    Explanation: Node 2 -> (3, 3, 2) is not good, because "3" 
+        is higher than it.
 
 Example 3:
 
-Input: root = [1]
-Output: 1
-Explanation: Root is considered as good.
+    Input: root = [1]
+    Output: 1
+    
+    Explanation: Root is considered as good.
 
 Constraints:
 
-The number of nodes in the binary tree is in the range [1, 10^5].
-Each node's value is between [-10^4, 10^4].
+    The number of nodes in the binary tree is in the range [1, 10^5].
+    Each node's value is between [-10^4, 10^4].
 
 Takeaway:
 
-YOu dont have to pass the whole path to the lower level
-you just need to pass the max value
-        
-We can use preorder traversal (dfs)
+    You dont have to pass the whole path to the lower level, you 
+        just need to pass the max value
 
-Root is always a good node and root is equal to root
+    We can use preorder traversal (dfs)
 
+    Root is always a good node and root is equal to root
 """
 
 # Definition for a binary tree node.
@@ -56,15 +57,16 @@ class TreeNode:
 
 class Solution:
 
-    def good_nodes(self, root: "TreeNode") -> int:
-        # first try with help of LLM
+    def goodNodes_(self, root: "TreeNode") -> int:
+        # first try
 
         # we need a mechanism to hold every path
-        #  for each node in the tree
+        # for each node in the tree
         # so that we can compare parents to children
+        
         # no we don't. Thats too complex
+        
         # good_nodes = [root]
-        # 
         # node_and_path = {root: [None]}
 
         # visit every node in the tree
@@ -88,15 +90,16 @@ class Solution:
             # right subtree
             right_count = dfs(node.right, max_val)
 
+            # what is the final return ? 
             return good_count + left_count + right_count
 
         return dfs(root, float("-inf"))
     
-    # neetcode
     def goodNodes(self, root: "TreeNode") -> int:
         
         # preorder traversal
         # dfs
+
         # pass the greatest value to the subtree
         # NOT all the values we have seen so far.
         # there is no need to do that.
@@ -111,6 +114,8 @@ class Solution:
             result = 1 if node.val >= max_value else 0
 
             max_value = max(max_value, node.val)
+
+            # increment result on recurrence
             result += dfs(node.left, max_value)
             result += dfs(node.right, max_value)
 
