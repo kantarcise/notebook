1f1ca267a3e932485f08cc38baa3100f9ca308ba
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-13 22:08:41 +0300

    The Perfect Hour :stopwatch:

--- neetcode/077.task_scheduler.py
+++ neetcode/077.task_scheduler.py
@@ -1,76 +1,120 @@
 """
-Given a characters array tasks, representing the tasks a 
-CPU needs to do, where each letter represents a different 
-task. Tasks could be done in any order. Each task is done 
-in one unit of time. For each unit of time, the CPU could 
-complete either one task or just be idle.
+Given a characters array tasks, representing the tasks a CPU needs 
+to do, where each letter represents a different task. 
 
-However, there is a non-negative integer n that represents 
-the cooldown period between two same tasks (the same letter 
-in the array), that is that there must be at least n units 
-of time between any two same tasks.
+Tasks could be done in any order. 
+
+Each task is done in one unit of time. 
+
+For each unit of time, the CPU could complete either one task or just be idle.
+
+However, there is a non-negative integer n that represents the cooldown 
+period between two same tasks (the same letter in the array), that is 
+that there must be at least n units of time between any two same tasks.
 
 Return the least number of units of times that the CPU will 
 take to finish all the given tasks.
 
 Example 1:
 
-Input: tasks = ["A","A","A","B","B","B"], n = 2
-Output: 8
-Explanation: 
-A -> B -> idle -> A -> B -> idle -> A -> B
-There is at least 2 units of time between any two same tasks.
+    Input: tasks = ["A","A","A","B","B","B"], n = 2
+    
+    Output: 8
+    
+    Explanation: 
+        
+        A -> B -> idle -> A -> B -> idle -> A -> B
+        There is at least 2 units of time between any two same tasks.
 
 Example 2:
 
-Input: tasks = ["A","A","A","B","B","B"], n = 0
-Output: 6
-Explanation: On this case any permutation of size 6 would 
-work since n = 0.
-["A","A","A","B","B","B"]
-["A","B","A","B","A","B"]
-["B","B","B","A","A","A"]
-...
-And so on.
+    Input: tasks = ["A","A","A","B","B","B"], n = 0
+
+    Output: 6
+
+    Explanation: 
+    
+        On this case any permutation of size 6 would 
+            work since n = 0.
+        
+        ["A","A","A","B","B","B"]
+        ["A","B","A","B","A","B"]
+        ["B","B","B","A","A","A"]
+        ...
+        And so on.
 
 Example 3:
 
-Input: tasks = ["A","A","A","A","A","A","B",
-    "C","D","E","F","G"], n = 2
-Output: 16
-Explanation: 
-One possible solution is
-A -> B -> C -> A -> D -> E -> A -> F -> G 
-    -> A -> idle -> idle -> A -> idle -> idle -> A
+    Input: tasks = ["A","A","A","A","A","A","B",
+        "C","D","E","F","G"], n = 2
+    
+    Output: 16
+    
+    Explanation: 
+        
+        One possible solution is
+        A -> B -> C -> A -> D -> E -> A -> F -> G 
+            -> A -> idle -> idle -> A -> idle -> idle -> A
 
 Constraints:
 
-    1 <= task.length <= 104
+    1 <= task.length <= 10^4
+
     tasks[i] is upper-case English letter.
+    
     The integer n is in the range [0, 100].
 
 Takeaway:
 
-we have a tasks list
-we need to make sure that there is at least 
-n difference between same tasks
-        
-we need to know the most frequent task
-because it will occur first in our solution
+    we have a tasks list
+    we need to make sure that there is at least 
+    n difference between same tasks
 
-we will use a max heap
-whichch task is most frequent - time complexity o(log n)
+    we need to know the most frequent task
+    because it will occur first in our solution
 
-we will also use a queue to hold the frequencies of tasks
-and the time they will be scheduled again
+    we will use a max heap
+    whichch task is most frequent - time complexity o(log n)
 
+    we will also use a queue to hold the frequencies of tasks
+    and the time they will be scheduled again
 """
 
 from collections import Counter, deque
 import heapq
 
 class Solution:
-    def findKthLargest(self, tasks: "list[int]", n: int) -> int:
+
+    def leastInterval__(self, tasks: "list[int]", n: int) -> int:
+        # does not work
+        
+        # edge case
+        if n == 0: return len(tasks)
+        
+        # most optimal way would be running 
+        # distinct tasks if possible
+        freq_map = {}
+        for elem in tasks:
+            freq_map[elem] = freq_map.get(elem, 0) + 1
+        
+        print(freq_map)
+        
+        length = 0
+        
+        # RuntimeError: dictionary changed size during iteration
+        # you cannot do that
+        
+        while freq_map:
+            for k in freq_map:
+                if freq_map[k] == 0:
+                    del freq_map[k]
+                    continue
+                freq_map[k] -= 1
+                length += 1
+                
+        return length
+
+    def leastInterval_(self, tasks: "list[int]", n: int) -> int:
         
         # we have a tasks list
         # we need to make sure that there is at least 
@@ -122,9 +166,13 @@ class Solution:
         # the tasks with intervals
         return time
 
-
-    # much smaller approach
     def leastInterval(self, tasks: "list[int]", n: int) -> int:
+        # much smaller code in size
+
+        # edge case
+        if n == 0:
+            return len(tasks)
+
         # Get the count of each task using Counter and
         #  store them in a list
         counts = list(Counter(tasks).values())
