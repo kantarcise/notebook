0e91ed9f32297dac8fd651a3c9a5efb184d87132
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-13 19:28:22 +0300

    The Perfect Hour :stopwatch:

--- neetcode/005.top_k_frequent_elements.py
+++ neetcode/005.top_k_frequent_elements.py
@@ -1,78 +1,65 @@
 """
-
 Given an integer array nums and an integer k, return the k most frequent 
 elements. You may return the answer in any order.
 
 Example 1:
 
-Input: nums = [1,1,1,2,2,3], k = 2
-Output: [1,2]
+    Input: nums = [1,1,1,2,2,3], k = 2
+    Output: [1,2]
 
 Example 2:
 
-Input: nums = [1], k = 1
-Output: [1]
+    Input: nums = [1], k = 1
+    Output: [1]
  
 Constraints:
 
-1 <= nums.length <= 105
--104 <= nums[i] <= 104
-k is in the range [1, the number of unique elements in the array].
-It is guaranteed that the answer is unique.
+    1 <= nums.length <= 10^5
+    -10^4 <= nums[i] <= 10^ 4
+    k is in the range [1, the number of unique elements in the array].
+    It is guaranteed that the answer is unique.
  
-Follow up: Your algorithm's time complexity must be better than O(n log n), where
- n is the array's size.
-
+Follow up: Your algorithm's time complexity must be better 
+than O(n log n), where n is the array's size.
 
 Takeaway:
 
-USE COLLECTIONS. A library that is written by competent 
-people is way better your weird approach.
+    USE COLLECTIONS. A library that is written by competent 
+    people is way better your weird approaches.
 
-COunter has most_common method.
+    COunter has most_common method.
 
     List the n most common elements and their counts from the most 
     common to the least. If n is None, then list all element counts.
 
-
 """
 
 from collections import Counter
+from heapq import nlargest
 
 class Solution:
-    def topKFrequent(self, nums, k: int):
+    
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        # we can use Counters with most_common
+        c = Counter(nums)
+        return [elem[0] for elem in c.most_common(k)]
+
+
+    def topKFrequent_(self, nums: list[int], k: int) -> list[int]:
+        # or we can take the heap route just to use, nlargest
+
+        # edge case 
+        if k == len(nums):
+            return nums
+
+        c = Counter(nums)
         
-        #  we need a data structure that holds certain keys and values
-        # the keys can be occurances of elements
-        # we willl access the result with a simple getitem
+        return nlargest(k, c.keys(), key = c.get)
 
-        count = {}
 
-        freq = [[] for i in range(len(nums) +1 )]
+sol = Solution()
+print(sol.topKFrequent(nums = [1,1,1,2,2,3], k = 2))
+print(sol.topKFrequent(nums = [1], k = 1))
 
-        for n in nums:
-            count[n] = 1 + count.get(n, 0)
-
-        for n, c in count.items():
-            freq[c].append(n)
-
-        res = []
-        for i in range(len(freq) - 1, 0, -1):
-            for n in freq[i]:
-                res.append(n)
-                if len(res) == k:
-                    return res
-
-    def top_k_frequent(self, nums, k):
-        
-        counts = Counter(nums)
-        
-        most_frequent = counts.most_common(k)
-
-        return [elem[0] for index, elem in enumerate(most_frequent)]
-        
-
-if __name__ == '__main__':
-    sol = Solution()
-    print(sol.topKFrequent([1,1,1,2,2,3], 2))
-    print(sol.top_k_frequent([1,1,1,2,2,3], 2))
+print(sol.topKFrequent_(nums = [1,1,1,2,2,3], k = 2))
+print(sol.topKFrequent_(nums = [1], k = 1))
