9a68b0894418472f7f352f22f284cf985ae9c632
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-14 00:23:22 +0300

    The Perfect Hour :stopwatch:

--- neetcode/135.regular_expression_matching.py
+++ neetcode/135.regular_expression_matching.py
@@ -9,32 +9,41 @@ The matching should cover the entire input string (not partial).
 
 Example 1:
 
-Input: s = "aa", p = "a"
-Output: false
+    Input: s = "aa", p = "a"
+    
+    Output: false
 
-Explanation: "a" does not match the entire string "aa".
+    Explanation: "a" does not match the entire string "aa".
 
 Example 2:
 
-Input: s = "aa", p = "a*"
-Output: true
-Explanation: '*' means zero or more of the preceding 
+    Input: s = "aa", p = "a*"
+    
+    Output: true
+    
+    Explanation: 
+        
+        '*' means zero or more of the preceding 
         element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
 
 Example 3:
 
-Input: s = "ab", p = ".*"
-Output: true
+    Input: s = "ab", p = ".*"
+    
+    Output: true
 
-Explanation: ".*" means "zero or more (*) of any character (.)".
+    Explanation: ".*" means "zero or more (*) of any character (.)".
  
-
 Constraints:
 
     1 <= s.length <= 20
+    
     1 <= p.length <= 20
+    
     s contains only lowercase English letters.
+    
     p contains only lowercase English letters, '.', and '*'.
+    
     It is guaranteed for each appearance of the character '*', there 
         will be a previous valid character to match.
 
@@ -43,10 +52,9 @@ Takeaway:
 DP questions start with a decision tree it seems.
 
 After you find the brute force solution, you try to optimize it.
-
-
 """
 class Solution:
+    
     def isMatch(self, s: str, p: str) -> bool:
         # "." can be anything
         # "*" zero or more of the preceeding elements
@@ -203,24 +211,3 @@ class Solution:
             return False
         
         return dfs(0, 0)
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
