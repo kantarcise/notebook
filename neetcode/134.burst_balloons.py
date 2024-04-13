0c97d3314761ceb6b2ea446e57e99f97bd75c1a5
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-14 00:22:03 +0300

    The Perfect Hour :stopwatch:

--- neetcode/134.burst_balloons.py
+++ neetcode/134.burst_balloons.py
@@ -1,53 +1,59 @@
 """
-You are given n balloons, indexed from 0 to n - 1. Each balloon is 
-painted with a number on it represented by an array nums. You are asked 
-to burst all the balloons.
+You are given n balloons, indexed from 0 to n - 1. 
+
+Each balloon is painted with a number on it represented by an array nums. 
+
+You are asked to burst all the balloons.
 
 If you burst the ith balloon, you will get 
-nums[i - 1] * nums[i] * nums[i + 1] coins. 
+
+    nums[i - 1] * nums[i] * nums[i + 1] 
+    
+coins. 
 
 If i - 1 or i + 1 goes out of bounds of the array, then 
 treat it as if there is a balloon with a 1 painted on it.
 
-Return the maximum coins you can collect by 
-bursting the balloons wisely.
+Return the maximum coins you can collect by bursting the balloons wisely.
 
 Example 1:
 
-Input: nums = [3,1,5,8]
-Output: 167
+    Input: nums = [3,1,5,8]
+    
+    Output: 167
 
-Explanation:
+    Explanation:
 
-    nums = [3,1,5,8] --> [3,5,8] --> [3,8] --> [8] --> []
-    coins =  3*1*5    +   3*5*8   +  1*3*8  + 1*8*1 = 167
+        nums = [3,1,5,8] --> [3,5,8] --> [3,8] --> [8] --> []
+        coins =  3*1*5    +   3*5*8   +  1*3*8  + 1*8*1 = 167
 
 Example 2:
 
-Input: nums = [1,5]
-Output: 10
+    Input: nums = [1,5]
+    
+    Output: 10
  
-
 Constraints:
 
     n == nums.length
+    
     1 <= n <= 300
+    
     0 <= nums[i] <= 100
 
 Takeaway:
 
-Still just using dfs - cached.
+    Still just using dfs - cached.
 
-Instead of thinking which balloon to pop first, think which 
-one to pop last.
+    Instead of thinking which balloon to pop first, think which 
+    one to pop last.
 
-Use the example to derive the relation of recursion
-
+    Use the example to derive the relation of recursion
 """
 class Solution:
     
     def maxCoins(self, nums: list[int]) -> int:
-        # neet
+        # works
 
         # a legit hard question
         # couple of tricks 
