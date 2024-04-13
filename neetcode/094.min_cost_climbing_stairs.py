ae92af4284259774b4b73c0e11f74df4ac2ae50b
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-13 22:43:24 +0300

    The Perfect Hour :stopwatch:

--- neetcode/094.min_cost_climbing_stairs.py
+++ neetcode/094.min_cost_climbing_stairs.py
@@ -1,7 +1,8 @@
 """
-You are given an integer array cost where cost[i] is 
-the cost of ith step on a staircase. Once you pay the 
-cost, you can either climb one or two steps.
+You are given an integer array cost where cost[i] is the cost 
+of ith step on a staircase. 
+
+Once you pay the cost, you can either climb one or two steps.
 
 You can either start from the step with index 0, or the 
 step with index 1.
@@ -10,74 +11,82 @@ Return the minimum cost to reach the top of the floor.
 
 Example 1:
 
-Input: cost = [10,15,20]
-Output: 15
-Explanation: You will start at index 1.
-- Pay 15 and climb two steps to reach the top.
-The total cost is 15.
+    Input: cost = [10,15,20]
+    
+    Output: 15
+    
+    Explanation: 
+    
+        You will start at index 1.
+        - Pay 15 and climb two steps to reach the top.
+        The total cost is 15.
 
 Example 2:
 
-Input: cost = [1,100,1,1,1,100,1,1,100,1]
-Output: 6
-Explanation: You will start at index 0.
-- Pay 1 and climb two steps to reach index 2.
-- Pay 1 and climb two steps to reach index 4.
-- Pay 1 and climb two steps to reach index 6.
-- Pay 1 and climb one step to reach index 7.
-- Pay 1 and climb two steps to reach index 9.
-- Pay 1 and climb one step to reach the top.
-The total cost is 6.
+    Input: cost = [1,100,1,1,1,100,1,1,100,1]
+    
+    Output: 6
+    
+    Explanation: 
+    
+        You will start at index 0.
+        - Pay 1 and climb two steps to reach index 2.
+        - Pay 1 and climb two steps to reach index 4.
+        - Pay 1 and climb two steps to reach index 6.
+        - Pay 1 and climb one step to reach index 7.
+        - Pay 1 and climb two steps to reach index 9.
+        - Pay 1 and climb one step to reach the top.
+        The total cost is 6.
 
 Constraints:
 
-2 <= cost.length <= 1000
-0 <= cost[i] <= 999
+    2 <= cost.length <= 1000
+    0 <= cost[i] <= 999
 
 Takeaway:
 
-the idea is from leaf to root
-we can use the list that is given to us
-and two variables
+    the idea is from leaf to root
+    we can use the list that is given to us
+    and two variables
 
-the calculation is a decision tree.
-[10, 15, 20] 
-        
-          0
-        /   \
-      1       2 
-     / \     /
-    2   3   3
-   /
-  3 
+    the calculation is a decision tree.
+    [10, 15, 20] 
 
-edges are costs of moving:
+              0
+            /   \
+          1       2 
+         / \     /
+        2   3   3
+       /
+      3 
 
-           0
-       10/   \10
-       1       2 
-    15/\15    20/
-     2   3   3
-  20/
-   3 
-        
-a lot of repeated work
-while we are completing the left subtree, we learn a lot about
-to go from 2  to 3 and 1 to 2
-SO lets use that info!
+    edges are costs of moving:
 
-my approach
-we make decisions at each step
-starting from the end, a lot of recomputation
-        
-starting from the last two steps, go backwards
-always compare 
+               0
+           10/   \10
+           1       2 
+        15/\15    20/
+         2   3   3
+      20/
+       3 
+
+    a lot of repeated work
+    while we are completing the left subtree, we learn a lot about
+    to go from 2  to 3 and 1 to 2
+    SO lets use that info!
+
+    my approach
+    we make decisions at each step
+    starting from the end, a lot of recomputation
+
+    starting from the last two steps, go backwards
+    always compare 
         
 """
 class Solution:
 
-    def minCostClimbingStairs(self, cost: list[int]) -> int:
-        # my approach
+    def minCostClimbingStairs_(self, cost: list[int]) -> int:
+        # my approach, works
         # we make decisions at each step
         # starting from the end, a lot of recomputation
         # we can use dp
