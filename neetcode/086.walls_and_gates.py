5b783d7f9cd9f7e827682d5b9bb920499582522e
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-13 22:27:35 +0300

    The Perfect Hour :stopwatch:

--- neetcode/086.walls_and_gates.py
+++ neetcode/086.walls_and_gates.py
@@ -1,69 +1,67 @@
 """
-You are given a m x n 2D grid initialized with 
-these three possible values.
+You are given a m x n 2D grid initialized with these three possible values.
 
- -1 - A wall or an obstacle.
+    -1 - A wall or an obstacle.
 
- 0 - A gate.
+    0 - A gate.
 
- INF - Infinity means an empty room. We use the 
-    value 2^31 - 1 = 2147483647 to represent INF as 
-    you may assume that the distance to a gate 
-    is less than 2147483647.
+    INF - Infinity means an empty room. We use the 
+       value 2^31 - 1 = 2147483647 to represent INF as 
+       you may assume that the distance to a gate 
+       is less than 2147483647.
 
-Fill each empty room with the distance to its 
-nearest gate. If it is impossible to reach a 
-Gate, that room should remain filled with INF
+Fill each empty room with the distance to its nearest gate. 
+
+If it is impossible to reach a Gate, that room should remain filled with INF
 
 Example 1:
 
-Input: [[2147483647, -1, 0, 2147483647],
-        [2147483647, 2147483647, 2147483647, -1],
-        [2147483647, -1, 2147483647,- 1],
-        [0, -1, 2147483647, 2147483647]]
+    Input: [[2147483647, -1, 0, 2147483647],
+            [2147483647, 2147483647, 2147483647, -1],
+            [2147483647, -1, 2147483647,- 1],
+            [0, -1, 2147483647, 2147483647]]
 
-Output: [[3, -1, 0, 1],
-         [2, 2, 1, -1],
-         [1, -1, 2, -1],
-         [0, -1, 3, 4]]
+    Output: [[3, -1, 0, 1],
+             [2, 2, 1, -1],
+             [1, -1, 2, -1],
+             [0, -1, 3, 4]]
 
-Explanation:
+    Explanation:
 
-the 2D grid is:
+        the 2D grid is:
 
-INF  -1  0  INF
+        INF  -1  0  INF
 
-INF INF INF  -1
+        INF INF INF  -1
 
-INF  -1 INF  -1
+        INF  -1 INF  -1
 
-  0  -1 INF INF
+          0  -1 INF INF
 
-the answer is:
+    the answer is:
 
-  3  -1   0   1
+          3  -1   0   1
 
-  2   2   1  -1
+          2   2   1  -1
 
-  1  -1   2  -1
+          1  -1   2  -1
 
-  0  -1   3   4
+          0  -1   3   4
 
 Example 2:
 
-Input: [[0, -1],
-        [2147483647, 2147483647]]
+    Input: [[0, -1],
+            [2147483647, 2147483647]]
 
-Output: [[0, -1],
-         [1, 2]]
+    Output: [[0, -1],
+             [1, 2]]
 
 Takeaway:
 
-BFS is cool to use. Because if we tried to do DFS
-calculations would mix from gates at different coordinates.
+    BFS is cool to use. Because if we tried to do DFS
+    calculations would mix from gates at different coordinates.
 
-With BFS, there comes a deque and set :)
-
+    With BFS, there comes a deque and set :)
 
 """
 from collections import deque
