49caabd5ef75409b75fb27cf868f25f6c0c25e46
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-13 22:00:48 +0300

    The Perfect Hour :stopwatch:

--- neetcode/074.last_stone_weight.py
+++ neetcode/074.last_stone_weight.py
@@ -2,58 +2,68 @@
 You are given an array of integers stones where 
 stones[i] is the weight of the ith stone.
 
-We are playing a game with the stones. On each turn, we 
-choose the heaviest two stones and smash them together. 
-Suppose the heaviest two stones have weights x and y 
-with x <= y. The result of this smash is:
+We are playing a game with the stones. 
+
+On each turn, we choose the heaviest two stones and smash them together. 
+
+Suppose the heaviest two stones have weights x and y with x <= y. 
+
+The result of this smash is:
 
     If x == y, both stones are destroyed, and
+    
     If x != y, the stone of weight x is destroyed, and the stone 
-of weight y has new weight y - x.
+        of weight y has new weight y - x.
 
 At the end of the game, there is at most one stone left.
 
-Return the weight of the last remaining stone. If 
-there are no stones left, return 0.
+Return the weight of the last remaining stone. 
+
+If there are no stones left, return 0.
 
 Example 1:
 
-Input: stones = [2,7,4,1,8,1]
-Output: 1
-Explanation: 
-We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
+    Input: stones = [2,7,4,1,8,1]
+    
+    Output: 1
+    
+    Explanation: 
+        
+        We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
 
-we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
+        we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
 
-we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+        we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
 
-we combine 1 and 1 to get 0 so the array converts to [1] then 
-that's the value of the last stone.
+        we combine 1 and 1 to get 0 so the array converts to [1] then 
+        that's the value of the last stone.
 
 Example 2:
 
-Input: stones = [1]
-Output: 1
+    Input: stones = [1]
+    
+    Output: 1
  
 Constraints:
 
-1 <= stones.length <= 30
-1 <= stones[i] <= 1000
+    1 <= stones.length <= 30
+    
+    1 <= stones[i] <= 1000
 
 Takeaway:
 
-if we use a sorted approach, we have to sort 
-the list every time
+    if we use a sorted approach, we have to sort 
+    the list every time
 
-use a max heap, 
-heapify takes o(n)
-every time accessing max heap is o(log n) - possibly 
-running n times
+    use a max heap, 
+    heapify takes o(n)
+    every time accessing max heap is o(log n) - possibly 
+    running n times
 
-to make a max heap in Python, just multiply 
-every value with -1
+    to make a max heap in Python, just multiply 
+    every value with -1
 
-in the calculations, use the example in your mind.
+    in the calculations, use the example in your mind.
 
 """
 
@@ -61,9 +71,9 @@ from heapq import heapify, heappop, heappush
 
 class Solution:
 
-    # brute force approach
-    # did not work
-    def last_stone_weight(self, stones: "list[int]") -> int:
+    def lastStoneWeight_(self, stones: "list[int]") -> int:
+        # brute force approach
+        # did not work
         def smash(seq):
             if len(seq) == 1:
                 return seq
