9d1a85f80fa5e4fdab0c3763f344d85c72b92bca
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-13 18:47:36 +0300

    The Perfect Hour :stopwatch:

--- neetcode/052.lowest_common_ancestor_of_binary_tree.py
+++ neetcode/052.lowest_common_ancestor_of_binary_tree.py
@@ -2,59 +2,60 @@
 Given a binary search tree (BST), find the lowest common 
 ancestor (LCA) node of two given nodes in the BST.
 
-According to the definition of LCA on Wikipedia: “The
- lowest common ancestor is defined between two nodes p 
- and q as the lowest node in T that has both p and q as
-descendants (where we allow a node to be a descendant of itself).”
+According to the definition of LCA on Wikipedia: 
+
+“The lowest common ancestor is defined between two nodes p and q 
+as the lowest node in T that has both p and q as descendants (where 
+we allow a node to be a descendant of itself).”
 
 Example 1:
 
-Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
-Output: 6
-Explanation: The LCA of nodes 2 and 8 is 6.
+    Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+    Output: 6
+    Explanation: The LCA of nodes 2 and 8 is 6.
 
 Example 2:
 
-Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
-Output: 2
-Explanation: The LCA of nodes 2 and 4 is 2, since a node is always
-can be a descendant of itself according to the LCA definition.
+    Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+    Output: 2
+    Explanation: The LCA of nodes 2 and 4 is 2, since a node is always
+    can be a descendant of itself according to the LCA definition.
 
 Example 3:
 
-Input: root = [2,1], p = 2, q = 1
-Output: 2
+    Input: root = [2,1], p = 2, q = 1
+    Output: 2
  
 Constraints:
 
-The number of nodes in the tree is in the range [2, 105].
--109 <= Node.val <= 109
-All Node.val are unique.
-p != q
-p and q will exist in the BST.
+    The number of nodes in the tree is in the range [2, 10^5].
+    -10^9 <= Node.val <= 10^9
+    All Node.val are unique.
+    p != q
+    p and q will exist in the BST.
 
 Takeaway:
 
-Its a binary search tree so the values are really useful.
+    Its a binary search tree so the values are really useful.
 
-if both of the values are smaller than root.val
-go to left subtree
-        
-if both of the values are larger than root.val
-go to right subtree
+    if both of the values are smaller than root.val
+    go to left subtree
 
-if one is bigger and one is smaller than root.val
-the anchestor will be the split happens in the tree
-the LCA is found, so to speak
+    if both of the values are larger than root.val
+    go to right subtree
 
-If we bump into the node, starting from the the root
-that is the LCA because nothing below will be 
-the common ancestor 
+    if one is bigger and one is smaller than root.val
+    the anchestor will be the split happens in the tree
+    the LCA is found, so to speak
 
-time complexity is the height of the tree, o(log n)
-because we are only visiting single node at each level
+    If we bump into the node, starting from the the root
+    that is the LCA because nothing below will be 
+    the common ancestor 
 
-We can also approach the question recursively
+    time complexity is the height of the tree, o(log n)
+    because we are only visiting single node at each level
+
+    We can also approach the question recursively
 
 """
 
@@ -67,74 +68,41 @@ class TreeNode:
 
 class Solution:
 
-    # this did not work
-    #   doesn't handle cases where the lowest
-    #   common ancestor is one of the nodes itself.
-    def lowest_common_anchestor(self, root, p, q):
-        # my first try
-        # lets make a hashmap where we hold all ancestors 
-        # for every node
-        # Helper function to perform a depth-first search and 
-        # record ancestors in a dictionary.
-        def dfs(node, parent=None):
-            if node:
-                anchestors[node] = parent
-                dfs(node.left, node)
-                dfs(node.right, node)
-
-        # Initialize the dictionary to store ancestors.
-        anchestors = {}
-        # Perform DFS to populate the ancestors dictionary.
-        dfs(root)
-
-        # Initialize sets to store ancestors of p and q.
-        ancestors_p = set()
-        ancestors_q = set()
-
-        # Build the set of ancestors for p.
-        while p in anchestors:
-            ancestors_p.add(p)
-            p = anchestors[p]
-
-        # Build the set of ancestors for q.
-        while q in anchestors:
-            ancestors_q.add(q)
-            q = anchestors[q]
-
-        # Find the lowest common ancestor by checking
-        #  common ancestors between p and q.
-        common_ancestors = ancestors_p.intersection(ancestors_q)
-        return common_ancestors.pop()
-
-    # llm approach
-    def lowest_common_ancestor(self, root, p, q):
+    def lowestCommonAncestor__(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # llm approach
         
         def findLCA(node):
             if not node:
                 return None
-            # how does he know what p or q are?
+            
+            # if the node is one of p or q, we found the answer!
             if node == p or node == q:
                 return node
             
+            # recur on left and right
             left = findLCA(node.left)
             right = findLCA(node.right)
             
+            # if they both returned a node
             if left and right:
                 return node
+            
+            # if only left or right returned
             if left:
                 return left
+            
             if right:
                 return right
             
+            # None of them returned
             return None
         
         return findLCA(root)
 
-    # neetcode
-    def lowestCommonAncestor(self, root, p, q):
-        
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         # use the values of the nodes.
-        
+        # this is way faster
+
         # if both of the values are smaller than root.val
         # go to left subtree
         
@@ -164,14 +132,15 @@ class Solution:
                 return current
 
 
-    # cool recursive approach
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        
+    def lowestCommonAncestor__(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # cool recursive approach
+        # explicitly calling the method ourselves
+
         if p.val < root.val and q.val < root.val:
-            return self.lowestCommonAncestor(root.left, p, q)
+            return self.lowestCommonAncestor__(root.left, p, q)
         # If the value of p and q is greater than root, then LCA will be in the right subtree
         elif p.val > root.val and q.val > root.val:
-            return self.lowestCommonAncestor(root.right, p, q)
+            return self.lowestCommonAncestor__(root.right, p, q)
         # If one value is less and the other is greater, then root is the LCA
         else:
             return root
