322469bd69b8f0908d4e07a6fa3d23efa5b688e0
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-13 20:27:05 +0300

    The Perfect Hour :stopwatch:

--- neetcode/061.implement_trie_prefix_tree.py
+++ neetcode/061.implement_trie_prefix_tree.py
@@ -1,79 +1,81 @@
-# TODO
-
 """
 Trie - Prefix Tree
 
-A trie (pronounced as "try") or prefix tree is a tree data 
-structure used to efficiently store and retrieve keys in a 
-dataset of strings. There are various applications of this 
-data structure, such as autocomplete and spellchecker.
+A trie (pronounced as "try") or prefix tree is a tree data structure 
+used to efficiently store and retrieve keys in a dataset of strings. 
+
+There are various applications of this data structure, such as 
+autocomplete and spellchecker.
 
 Implement the Trie class:
 
-Trie(): Initializes the trie object.
+    Trie(): Initializes the trie object.
 
-void insert(String word): Inserts the string word into the trie.
+    void insert(String word): Inserts the string word into the trie.
 
-boolean search(String word): Returns true if the string 
-word is in the trie (i.e., was inserted before), and 
-false otherwise. 
+    boolean search(String word): Returns true if the string 
+        word is in the trie (i.e., was inserted before), and 
+        false otherwise. 
 
-boolean startsWith(String prefix): Returns true if there 
-is a previously inserted string word that has the 
-prefix prefix, and false otherwise.
+    boolean startsWith(String prefix): Returns true if there 
+        is a previously inserted string word that has the 
+        prefix prefix, and false otherwise.
  
 Example 1:
 
-Input
-["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
-[[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+    Input:
 
-Output
-[null, null, true, false, true, null, true]
+    ["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+    [[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
 
-Explanation
+    Output:
+    [null, null, true, false, true, null, true]
 
-Trie trie = new Trie();
-trie.insert("apple");
-trie.search("apple");   // return True
-trie.search("app");     // return False
-trie.startsWith("app"); // return True
-trie.insert("app");
-trie.search("app");     // return True
- 
+    Explanation:
+
+        Trie trie = new Trie();
+        trie.insert("apple");
+        trie.search("apple");   // return True
+        trie.search("app");     // return False
+        trie.startsWith("app"); // return True
+        trie.insert("app");
+        trie.search("app");     // return True
+
 Constraints:
 
-1 <= word.length, prefix.length <= 2000
-Word and prefix consist only of lowercase English letters.
-At most 3 * 104 calls in total will be made to insert, search, and startsWith.
+    1 <= word.length, prefix.length <= 2000
+    
+    Word and prefix consist only of lowercase English letters.
+    
+    At most 3 * 10^4 calls in total will be made to insert, search, and startsWith.
 
 Takeaway:
 
-We can start with making a TrieNode for the class.
-Each node will have child nodes, and at each word ending 
-we will have a Flag indicating the end.
-
-Using a Trie data structure, we have efficient 
-string search and prefix matching.
-
-The time complexity for inserting, searching, and 
-checking prefixes in a Trie is O(L), where L is the length 
-of the word or prefix, making it an efficient choice 
-for string-related tasks.
-
-For different words we will be using a lot of the same nodes
-
-.
- \ 
-  a
-   \ 
-    p 
-     \
-      p 
-     / \ 
-    e   l 
-   /      \ 
-  n        e
+    We can start with making a TrieNode for the class.
+    Each node will have child nodes, and at each word ending 
+    we will have a Flag indicating the end.
+    
+    Using a Trie data structure, we have efficient 
+    string search and prefix matching.
+    
+    The time complexity for inserting, searching, and 
+    checking prefixes in a Trie is O(L), where L is the length 
+    of the word or prefix, making it an efficient choice 
+    for string-related tasks.
+    
+    For different words we will be using a lot of the same nodes
+    
+    .
+     \ 
+      a
+       \ 
+        p 
+         \
+          p 
+         / \ 
+        e   l 
+       /      \ 
+      n        e
 
 """
 
