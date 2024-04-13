5cfe44163c2c22c97df32bbf25d07ded4e7276f2
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-13 23:54:20 +0300

    The Perfect Hour :stopwatch:

--- neetcode/122.swim_in_rising_water.py
+++ neetcode/122.swim_in_rising_water.py
@@ -6,50 +6,71 @@ at that point (i, j).
 The rain starts to fall. 
 
 At time t, the depth of the  water everywhere is t.
+
 You can swim from a square to another 4-directionally 
 adjacent square if and only if the elevation of both 
 squares individually are at most t. 
 
 You can swim infinite distances in zero time. 
+
 Of course, you must stay within the boundaries of 
 the grid during your swim.
 
-Return the least time until you can reach 
-the bottom right square (n - 1, n - 1) 
-if you start at the top left square (0, 0).
+Return the least time until you can reach the bottom right 
+square (n - 1, n - 1) if you start at the top left square (0, 0).
 
 Example 1:
 
-Input: grid = [[0,2],[1,3]]
+    Input: grid = [[0,2],[1,3]]
 
-Output: 3
-Explanation:
-At time 0, you are in grid location (0, 0).
-You cannot go anywhere else because 4-directionally adjacent neighbors have a higher elevation than t = 0.
-You cannot reach point (1, 1) until time 3.
-When the depth of water is 3, we can swim anywhere inside the grid.
+    Output: 3
+    
+    Explanation:
+    
+        At time 0, you are in grid location (0, 0).
+    
+        You cannot go anywhere else because 4-directionally adjacent 
+            neighbors have a higher elevation than t = 0.
+        
+        You cannot reach point (1, 1) until time 3.
+        
+        When the depth of water is 3, we can swim anywhere 
+            inside the grid.
 
 Example 2:
 
-Input: grid = [[0,1,2,3,4],[24,23,22,21,5],[12,13,14,15,16],[11,17,18,19,20],[10,9,8,7,6]]
-Output: 16
-Explanation: The final route is shown.
-We need to wait until time 16 so that (0, 0) and (4, 4) are connected.
+    
+    Input: grid = [[0,1,2,3,4],
+                   [24,23,22,21,5],
+                   [12,13,14,15,16],
+                   [11,17,18,19,20],
+                   [10,9,8,7,6]]
+    
+    Output: 16
+    
+    Explanation: 
+
+        The final route is shown (website).
+    
+        We need to wait until time 16 so that (0, 0) and (4, 4) are connected.
 
 Constraints:
 
-n == grid.length
-n == grid[i].length
-1 <= n <= 50
-0 <= grid[i][j] < n2
-Each value grid[i][j] is unique.
+    n == grid.length
+    
+    n == grid[i].length
+    
+    1 <= n <= 50
+    
+    0 <= grid[i][j] < n2
+    
+    Each value grid[i][j] is unique.
 
 Takeaway:
 
-Djikstra is basically a BFS but 
-instead of a normal queue
-we use a priority queue, which is a heap in Python
-
+    Djikstra is basically a BFS but 
+        instead of a normal queue
+        we use a priority queue, which is a heap in Python
 """
 
 from heapq import heappop, heappush
@@ -124,5 +145,3 @@ class Solution:
                          [max(t, grid[nei_row][nei_col]),
                           nei_row,
                           nei_col])
-                    
-            
