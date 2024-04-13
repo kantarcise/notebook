0e0e65ed2f2019c304d9403d65e09ba1d0949345
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-14 00:46:19 +0300

    The Perfect Hour :stopwatch:

--- neetcode/148.pow_x_n.py
+++ neetcode/148.pow_x_n.py
@@ -4,45 +4,55 @@ the power n (i.e., xn).
 
 Example 1:
 
-Input: x = 2.00000, n = 10
-Output: 1024.00000
+    Input: x = 2.00000, n = 10
+    
+    Output: 1024.00000
 
 Example 2:
 
-Input: x = 2.10000, n = 3
-Output: 9.26100
+    Input: x = 2.10000, n = 3
+    
+    Output: 9.26100
 
 Example 3:
 
-Input: x = 2.00000, n = -2
-Output: 0.25000
-Explanation: 2-2 = 1/22 = 1/4 = 0.25
+    Input: x = 2.00000, n = -2
+    
+    Output: 0.25000
+    
+    Explanation: 
+        
+        2-2 = 1/22 = 1/4 = 0.25
 
 Constraints:
 
--100.0 < x < 100.0
--231 <= n <= 231-1
-n is an integer.
-Either x is not zero or n > 0.
--104 <= xn <= 104
+    -100.0 < x < 100.0
+    
+    -2^31 <= n <= 2^31-1
+    
+    n is an integer.
+    
+    Either x is not zero or n > 0.
+    
+    -10^4 <= xn <= 10^4
 
 Takeaway:
 
-If the power is negative, just return 1 / result
+    If the power is negative, just return 1 / result
 
-you can solve the problem, using recursion
+    you can solve the problem, using recursion
 
-condition is based on the power and it getting halved.
+    condition is based on the power and it getting halved.
 
-If you are writing a while block, you have 
-to exit it at some point.
-
+    If you are writing a while block, you have 
+    to exit it at some point.
 """
 
 class Solution:
+
     def myPow_(self, x: float, n: int) -> float:
         # cool idea
-        # did not work
+        # does not work
         
         # if n is negative
         # x ^ n  = 1 / x ^ n
