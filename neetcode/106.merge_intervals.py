ec60866336a9f14d61d8e9a39371de67d0e333cb
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-13 23:00:36 +0300

    The Perfect Hour :stopwatch:

--- neetcode/106.merge_intervals.py
+++ neetcode/106.merge_intervals.py
@@ -1,36 +1,41 @@
 """
 Given an array of intervals where 
-
 intervals[i] = [starti, endi], merge all overlapping 
 intervals, and return an array of the non-overlapping 
 intervals that cover all the intervals in the input.
 
 Example 1:
 
-Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
-Output: [[1,6],[8,10],[15,18]]
-Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+    Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+    
+    Output: [[1,6],[8,10],[15,18]]
+    
+    Explanation: 
+        Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
 
 Example 2:
 
-Input: intervals = [[1,4],[4,5]]
-Output: [[1,5]]
-Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+    Input: intervals = [[1,4],[4,5]]
+    
+    Output: [[1,5]]
+    
+    Explanation: Intervals [1,4] and [4,5] are considered overlapping.
 
 Constraints:
 
-1 <= intervals.length <= 104
-intervals[i].length == 2
-0 <= starti <= endi <= 104
+    1 <= intervals.length <= 10^4
+
+    intervals[i].length == 2
+
+    0 <= starti <= endi <= 10^4
 
 Takeaway:
 
-CALM DOWN and think about the problem 
+    CALM DOWN and think about the problem 
 
-list.sort(key = lambda ...) is a classic
+    list.sort(key = lambda ...) is a classic
 
-You can solve it in a single traverse.
-
+    You can solve it in a single traverse.
 """
 
 class Solution:
@@ -69,7 +74,6 @@ class Solution:
                 temp = [elem[0], elem[1]]
                 # ?
         return result
-        
         
     def merge(self, intervals: list[list[int]]) -> list[list[int]]:
         """Given the list of interval lists,
