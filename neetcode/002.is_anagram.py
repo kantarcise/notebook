cf760aeca9672381bf19e3d72fb86cd564aa6c7f
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-13 19:21:39 +0300

    The Perfect Hour :stopwatch:

--- neetcode/002.is_anagram.py
+++ neetcode/002.is_anagram.py
@@ -1,57 +1,65 @@
 """
 Given two strings s and t, return true if t is an
- anagram of s, and false otherwise.
+anagram of s, and false otherwise.
 
-An Anagram is a word or phrase formed by rearranging the letters
- of a different word or phrase, typically using all the original 
- letters exactly once.
-
+An Anagram is a word or phrase formed by rearranging the 
+letters of a different word or phrase, typically using 
+all the original letters exactly once.
  
-
 Example 1:
 
-Input: s = "anagram", t = "nagaram"
-Output: true
+    Input: s = "anagram", t = "nagaram"
+    Output: true
 
 Example 2:
 
-Input: s = "rat", t = "car"
-Output: false
+    Input: s = "rat", t = "car"
+    Output: false
  
 Constraints:
 
-1 <= s.length, t.length <= 5 * 104
-s and t consist of lowercase English letters.
+    1 <= s.length, t.length <= 5 * 10^4
+    s and t consist of lowercase English letters.
 
+Takeaway:
+
+    Counter is just made for the job!
 """
 
 from collections import Counter
 
-
 class Solution:
-    # my first solution
-    def isAnagram(self, s: str, t: str) -> bool:
+
+    def isAnagram__(self, s: str, t: str) -> bool:
+        # my first solution - brute force
         dict_1 = {character: s.count(character)  for character in set(s)} # o(n^2)
         dict_2 = {character: t.count(character)  for character in set(t)} # o(n^2)
         return dict_1 == dict_2
+    
+    def isAnagram_(self, s: str, t: str) -> bool:
+        # second solution - way better for bigger sized strings\
+        return Counter(s) == Counter(t)  
 
-    # second solution - way better for bigger sized strings\
+    def isAnagram___(self, s: str, t: str) -> bool:
+        # adding elements and comparing the dicts would work
+        map_1, map_2 = {}, {}
 
-    # A faster solution is to use the collections.Counter() function
-    # from the Python standard library. The Counter() function takes a
-    #  sequence as input and returns a dictionary that maps each
-    #  element in the sequence to its frequency.
-    def isAnagram(self, s: str, t: str) -> bool:
-        counter_1 = Counter(s) # o(n)
-        counter_2 = Counter(t) # o(n)
-        return counter_1 == counter_2
+        for elem in s:
+            map_1[elem] = map_1.get(elem, 0) + 1
 
-    # third solution - get dirty - o(n) time complexity
+        for elem in t:
+            map_2[elem] = map_2.get(elem, 0) + 1
+
+        return map_1 == map_2
+
     def isAnagram(self, s:str, t:str) -> bool:
+        # even faster one
+        # o(n) time complexity
+
         if len(s) != len(t):
             return False
 
-        # single dict 
+        # let's use only a single dict 
         char_count = {}
         for char in s:
             # if it already exists, add 1 to value
@@ -62,13 +70,17 @@ class Solution:
             if char not in char_count:
                 return False
             char_count[char] -= 1
+
+            # if we expire ther count of a character
             if char_count[char] == 0:
                 del char_count[char]
 
         # expecting an empty dict in the end
         return not char_count
 
+sol = Solution()
+print(sol.isAnagram(s = "rat", t  = "car")) # False
+print(sol.isAnagram(s = "anagram", t  = "nagaram")) # True
 
-if __name__ == '__main__':
-    sol = Solution()
-    print(sol.isAnagram("anagram","nagaram"))
+print(sol.isAnagramMap(s = "rat", t  = "car")) # False
+print(sol.isAnagramMap(s = "anagram", t  = "nagaram")) # True
