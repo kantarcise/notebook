1c749914e9027b90e4b817dbcb7471aa705f1703
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-13 19:38:51 +0300

    The Perfect Hour :stopwatch:

--- neetcode/009.longest_consecutive_sequence.py
+++ neetcode/009.longest_consecutive_sequence.py
@@ -1,100 +1,68 @@
 """
-Given an unsorted array of integers nums, return the length of the longest consecutive 
-elements sequence.
+Given an unsorted array of integers nums, return the length of the 
+longest consecutive elements sequence.
 
 You must write an algorithm that runs in O(n) time.
 
-
 Example 1:
 
-Input: nums = [100,4,200,1,3,2]
-Output: 4
-Explanation: The longest consecutive elements sequence is [1, 2, 3, 4].
+    Input: nums = [100,4,200,1,3,2]
+    Output: 4
+    Explanation: The longest consecutive elements sequence is [1, 2, 3, 4].
 
-     Therefore its length is 4.
+         Therefore its length is 4.
      
 Example 2:
 
-Input: nums = [0,3,7,2,5,8,4,6,0,1]
-Output: 9
+    Input: nums = [0,3,7,2,5,8,4,6,0,1]
+    Output: 9
  
 
 Constraints:
 
-0 <= nums.length <= 10^5
--10^9 <= nums[i] <= 10^9
-
+    0 <= nums.length <= 10^5
+    -10^9 <= nums[i] <= 10^9
 
 Takeaway:
 
-# to get rid of reapparent values, you can use a set,
+    To get rid of reapparent values, you can use a set,
 
-# imply defining the question in ENGLISH is the first step.
+    Imply defining the question in ENGLISH is the first step.
 
-you can override a value in an iteration with 
-    
-    current = max(current, new)
+    you can override a value in an iteration with 
+
+        current = max(current, new)
 
 """
 
 class Solution:
-    
-    def longestConsecutive(self, nums) -> int:
-        # My first try - it failed because I did not cover the streaks
-         
-        # in the list, we need to find all the elements
-        #  that has the difference of 1 between them
+    def longestConsecutive(self, nums: list[int]) -> int:
+        # o(n) means no sorting
         
-        # I can sort the list
-        # for each element difference between elements for one 
+        # we are interested in unique elements
+        nums_set = set(nums)
 
-        if not nums:
-            return 0
-        consecutive = 1
-
-        sorted_list = sorted(nums)
-
-        for i in range(len(sorted_list)-1):
-            if sorted_list[i+1] - sorted_list[i] == 1:
-                consecutive +=1
-
-        return consecutive
-
-    def longestConsecutiveSecond(self, nums) -> int:
-
-        nums_set = set(nums)  # Convert the list to a set for faster lookup
-        
+        # we are only interested in longest streak
         longest_streak = 0
         
         for num in nums_set:
-            # Check if num is the start of a consecutive subsequence
             if num - 1 not in nums_set:
+                # a new streak starts
                 current_num = num
                 current_streak = 1
-
-                # Find the length of the consecutive subsequence
+                
                 while current_num + 1 in nums_set:
+                    # streak continues
                     current_num += 1
-                    current_streak += 1
+                    current_streak +=1
 
-                # override longest_streak for each consecutive subsequence
-                longest_streak = max(longest_streak, current_streak)
-
+                # update the longest streak         
+                longest_streak = max(current_streak, 
+                                        longest_streak)
+                
         return longest_streak
 
+sol = Solution()
 
-if __name__ == '__main__':
-    
-    prod = Solution()
-
-    print("This is my first approach")
-    print("This should be 9: ", prod.longestConsecutive([0,3,7,2,5,8,4,6,0,1]))
-    print("This should be 7: ", prod.longestConsecutive([9,1,4,7,3,-1,0,5,8,-1,6]))
-    print("This should be 0: ", prod.longestConsecutive([]))
-
-    print("This is my second approach")
-    print("Which works")
-
-    print("This should be 9: ", prod.longestConsecutiveSecond([0,3,7,2,5,8,4,6,0,1]))
-    print("This should be 7: ", prod.longestConsecutiveSecond([9,1,4,7,3,-1,0,5,8,-1,6]))
-    print("This should be 0: ", prod.longestConsecutiveSecond([]))
+print(sol.longestConsecutive(nums = [100,4,200,1,3,2])) # 4
+print(sol.longestConsecutive(nums = [0,3,7,2,5,8,4,6,0,1])) # 9
