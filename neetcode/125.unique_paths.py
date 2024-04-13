7a08fbafce4fa87f892cf712562088c05738003c
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-13 23:59:17 +0300

    The Perfect Hour :stopwatch:

--- neetcode/125.unique_paths.py
+++ neetcode/125.unique_paths.py
@@ -1,47 +1,49 @@
 """
-There is a robot on an m x n grid. The robot is initially located 
-at the top-left corner (i.e., grid[0][0]). The robot tries 
-to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). 
+There is a robot on an m x n grid. 
+
+The robot is initially located at the top-left corner (i.e., grid[0][0]). 
+
+The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). 
+
 The robot can only move either down or right at any point in time.
 
-Given the two integers m and n, return the number of 
-possible unique paths that the robot can take to 
-reach the bottom-right corner.
+Given the two integers m and n, return the number of possible unique 
+paths that the robot can take to reach the bottom-right corner.
 
 The test cases are generated so that the answer will 
-be less than or equal to 2 * 109.
+be less than or equal to 2 * 10^9.
 
 Example 1:
 
-Input: m = 3, n = 7
-Output: 28
+    Input: m = 3, n = 7
+    
+    Output: 28
 
 Example 2:
 
-Input: m = 3, n = 2
-Output: 3
+    Input: m = 3, n = 2
+    
+    Output: 3
 
-Explanation: From the top-left corner, 
-    there are a total of 3 ways to reach the bottom-right corner:
+    Explanation: From the top-left corner, 
+        there are a total of 3 ways to reach the bottom-right corner:
 
-    1. Right -> Down -> Down
+        1. Right -> Down -> Down
 
-    2. Down -> Down -> Right
+        2. Down -> Down -> Right
 
-    3. Down -> Right -> Down
+        3. Down -> Right -> Down
  
-
 Constraints:
 
     1 <= m, n <= 100
 
 Takeaway:
 
-It is a 2D DP problem.
+    It is a 2D DP problem.
 
-Literally a math problem, we add bottom and right
-to get the value on current tile!
-
+    Literally a math problem, we add bottom and right
+    to get the value on current tile!
 """
 
 class Solution:
