f3846a645f15fad4aaa6296008465592baaab5ce
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-13 19:42:55 +0300

    The Perfect Hour :stopwatch:

--- neetcode/011.two_sum_2_input_array_is_sorted.py
+++ neetcode/011.two_sum_2_input_array_is_sorted.py
@@ -6,34 +6,38 @@ up to a specific target number.
 Let these two numbers be numbers[index1] and numbers[index2] 
 where 1 <= index1 < index2 < numbers.length.
 
-Return the indices of the two numbers, index1 and index2, added by one
- as an integer array [index1, index2] of length 2.
+Return the indices of the two numbers, index1 and index2, added 
+by one as an integer array [index1, index2] of length 2.
 
-The tests are generated such that there is exactly one solution. You may
-not use the same element twice.
+The tests are generated such that there is exactly one solution. 
+
+You may not use the same element twice.
 
 Your solution must use only constant extra space.
 
 Example 1:
 
-Input: numbers = [2,7,11,15], target = 9
-Output: [1,2]
-Explanation: The sum of 2 and 7 is 9. 
-  Therefore, index1 = 1, index2 = 2. We return [1, 2].
+    Input: numbers = [2,7,11,15], target = 9
+    Output: [1,2]
+    
+    Explanation: The sum of 2 and 7 is 9. 
+      Therefore, index1 = 1, index2 = 2. We return [1, 2].
 
 Example 2:
 
-Input: numbers = [2,3,4], target = 6
-Output: [1,3]
-Explanation: The sum of 2 and 4 is 6. 
-  Therefore index1 = 1, index2 = 3. We return [1, 3].
+    Input: numbers = [2,3,4], target = 6
+    Output: [1,3]
+    
+    Explanation: The sum of 2 and 4 is 6. 
+      Therefore index1 = 1, index2 = 3. We return [1, 3].
 
 Example 3:
 
-Input: numbers = [-1,0], target = -1
-Output: [1,2]
-Explanation: The sum of -1 and 0 is -1. 
-  Therefore index1 = 1, index2 = 2. We return [1, 2].
+    Input: numbers = [-1,0], target = -1
+    Output: [1,2]
+    
+    Explanation: The sum of -1 and 0 is -1. 
+      Therefore index1 = 1, index2 = 2. We return [1, 2].
  
 Constraints:
 
@@ -45,10 +49,10 @@ Constraints:
 
 Takeaway:
 
-    - enumerate is still one of the best ways to 
+    Enumerate is still one of the best ways to 
       traverse a sequence.
 
-    - You can and you should use dicts whenever you want to store 
+    You can and you should use dicts whenever you want to store 
       and access a value/key combination.
 """
 class Solution():
@@ -102,7 +106,7 @@ class Solution():
         # here is just for practice
         
         numbers_to_index = {}
-        for index, number in enumerate(seq):
+        for index, number in enumerate(numbers):
             # find the complement
             complement = target - number
 
