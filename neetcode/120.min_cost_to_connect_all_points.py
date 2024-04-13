e3e3fbf5d7f238f9fe77665cc3ace1150abea093
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-13 23:51:22 +0300

    The Perfect Hour :stopwatch:

--- neetcode/120.min_cost_to_connect_all_points.py
+++ neetcode/120.min_cost_to_connect_all_points.py
@@ -6,36 +6,41 @@ The cost of connecting two points [xi, yi] and [xj, yj] is the
 manhattan distance between them: |xi - xj| + |yi - yj|, 
 where |val| denotes the absolute value of val.
 
-Return the minimum cost to make all points connected. All points 
-are connected if there is exactly one simple path between 
+Return the minimum cost to make all points connected. 
+
+All points are connected if there is exactly one simple path between 
 any two points.
 
 Example 1:
 
-Input: points = [[0,0],[2,2],[3,10],[5,2],[7,0]]
-Output: 20
-Explanation: 
+    Input: points = [[0,0],[2,2],[3,10],[5,2],[7,0]]
+    
+    Output: 20
+    
+    Explanation: 
 
-We can connect the points as shown above to get the minimum cost of 20.
-Notice that there is a unique path between every pair of points.
+        We can connect the points as shown above to get the minimum cost of 20.
+        Notice that there is a unique path between every pair of points.
 
 Example 2:
 
-Input: points = [[3,12],[-2,5],[-4,1]]
-Output: 18
+    Input: points = [[3,12],[-2,5],[-4,1]]
+    
+    Output: 18
 
 Constraints:
 
     1 <= points.length <= 1000
-    -106 <= xi, yi <= 106
+
+    -10^6 <= xi, yi <= 10^6
+
     All pairs (xi, yi) are distinct.
 
 Takeaway:
 
-MST for Prim's !
+    MST for Prim's !
 
-BFS - visit set, [cost, node] 
-
+    BFS - visit set, [cost, node] 
 """
 
 from heapq import heappop, heappush
