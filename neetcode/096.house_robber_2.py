2411d404630b4aa5fb37593aaa9ae7790c82043d
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-13 22:45:47 +0300

    The Perfect Hour :stopwatch:

--- neetcode/096.house_robber_2.py
+++ neetcode/096.house_robber_2.py
@@ -1,10 +1,14 @@
 """
-You are a professional robber planning to rob houses along a street. 
+You are a professional robber planning to rob houses along a street.
+
 Each house has a certain amount of money stashed. All houses at 
-this place are arranged in a circle. That means the first house is 
-the neighbor of the last one. Meanwhile, adjacent houses have a 
-security system connected, and it will automatically contact the 
-police if two adjacent houses were broken into on the same night.
+this place are arranged in a circle. 
+
+That means the first house is the neighbor of the last one. 
+
+Meanwhile, adjacent houses have a security system connected, and it 
+will automatically contact the police if two adjacent houses 
+were broken into on the same night.
 
 Given an integer array nums representing the amount of money of 
 each house, return the maximum amount of money you can rob 
@@ -12,36 +16,44 @@ tonight without alerting the police.
 
 Example 1:
 
-Input: nums = [2,3,2]
-Output: 3
-Explanation: You cannot rob house 1 (money = 2) and then 
-rob house 3 (money = 2), because they are adjacent houses.
+    Input: nums = [2,3,2]
+    
+    Output: 3
+    
+    Explanation: 
+    
+        You cannot rob house 1 (money = 2) and then 
+        rob house 3 (money = 2), because they are adjacent houses.
 
 Example 2:
 
-Input: nums = [1,2,3,1]
-Output: 4
-Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
-Total amount you can rob = 1 + 3 = 4.
+    Input: nums = [1,2,3,1]
+    
+    Output: 4
+    
+    Explanation: 
+        
+        Rob house 1 (money = 1) and then rob house 3 (money = 3).
+        Total amount you can rob = 1 + 3 = 4.
 
 Example 3:
 
-Input: nums = [1,2,3]
-Output: 3
+    Input: nums = [1,2,3]
+    Output: 3
 
 Constraints:
 
-1 <= nums.length <= 100
-0 <= nums[i] <= 1000
+    1 <= nums.length <= 100
+    
+    0 <= nums[i] <= 1000
 
 Takeaway:
 
-We either rob the first house or dont.
+    We either rob the first house or dont.
 
-basicaly the 3sum and 2sum relationship.
+    basicaly the 3sum and 2sum relationship.
 
-Instead of whole rewrites, we write a helper function.
-
+    Instead of whole rewrites, we write a helper function.
 """
 
 class Solution:
@@ -98,4 +110,3 @@ class Solution:
         
         # Either rob the first house or don't
         return max(rob_linear(nums[:-1]), rob_linear(nums[1:]))
-    
