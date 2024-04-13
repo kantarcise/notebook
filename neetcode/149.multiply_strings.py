756665c6fb16438502c9755dfc9ca502dcb90eb5
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-14 00:48:15 +0300

    The Perfect Hour :stopwatch:

--- neetcode/149.multiply_strings.py
+++ neetcode/149.multiply_strings.py
@@ -1,40 +1,43 @@
 """
-Given two non-negative integers num1 
-and num2 represented as strings, return the product 
-of num1 and num2, also represented as a string.
+Given two non-negative integers num1 and num2 represented 
+as strings, return the product of num1 and num2, also represented 
+as a string.
 
 Note: You must not use any built-in BigInteger library 
 or convert the inputs to integer directly.
 
 Example 1:
 
-Input: num1 = "2", num2 = "3"
-Output: "6"
+    Input: num1 = "2", num2 = "3"
+    
+    Output: "6"
 
 Example 2:
 
-Input: num1 = "123", num2 = "456"
-Output: "56088"
+    Input: num1 = "123", num2 = "456"
+    
+    Output: "56088"
  
-
 Constraints:
 
-1 <= num1.length, num2.length <= 200
-num1 and num2 consist of digits only.
-Both num1 and num2 do not contain any leading 
-zero, except the number 0 itself.
+    1 <= num1.length, num2.length <= 200
+    
+    num1 and num2 consist of digits only.
+    
+    Both num1 and num2 do not contain any leading 
+        zero, except the number 0 itself.
 
 Takeaway:
 
-You can int(str_digit) by updating the value of a number
+    You can int(str_digit) by updating the value of a number
 
-starting from 0 and multipling with 10 each time 
+    starting from 0 and multipling with 10 each time 
 
-and adding the rightmost value
-
+    and adding the rightmost value
 """
 
 class Solution:
+
     def multiply_(self, num1: str, num2: str) -> str:
         # this is not allowed.
         if num1 == "0" or num2 == "0":
@@ -63,8 +66,7 @@ class Solution:
         return str(n1 * n2)
         
     def multiply__(self, num1: str, num2: str) -> str:
-        # this is from neetcode
-        # just NOT good.
+        # works, but NOT good.
         
         if "0" in [num1, num2]:
             return "0"
