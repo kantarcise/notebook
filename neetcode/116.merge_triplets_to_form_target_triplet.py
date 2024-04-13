26185ef44c4e6c3b40d16b3baf55d09744994c20
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-13 23:19:24 +0300

    The Perfect Hour :stopwatch:

--- neetcode/116.merge_triplets_to_form_target_triplet.py
+++ neetcode/116.merge_triplets_to_form_target_triplet.py
@@ -1,7 +1,9 @@
 """
 A triplet is an array of three integers. 
+
 You are given a 2D integer array triplets, 
 where triplets[i] = [ai, bi, ci] describes the ith triplet. 
+
 You are also given an integer array target = [x, y, z] that 
 describes the triplet you want to obtain.
 
@@ -18,64 +20,76 @@ triplets any number of times (possibly zero):
 Return true if it is possible to obtain the target triplet [x, y, z] 
 as an element of triplets, or false otherwise.
 
-
 Example 1:
 
-Input: triplets = [[2,5,3],[1,8,4],[1,7,5]], target = [2,7,5]
+    Input: triplets = [[2,5,3],[1,8,4],[1,7,5]], target = [2,7,5]
 
-Output: true
+    Output: true
 
-Explanation: Perform the following operations:
+    Explanation: Perform the following operations:
 
-- Choose the first and last triplets [[2,5,3],[1,8,4],[1,7,5]]. 
-Update the last triplet to be 
-[max(2,1), max(5,7), max(3,5)] = [2,7,5]. 
-triplets = [[2,5,3],[1,8,4],[2,7,5]]
+        - Choose the first and last triplets [[2,5,3],[1,8,4],[1,7,5]]. 
+        
+        Update the last triplet to be 
+            [max(2,1), max(5,7), max(3,5)] = [2,7,5]. 
+        
+        triplets = [[2,5,3],[1,8,4],[2,7,5]]
 
-The target triplet [2,7,5] is now an element of triplets.
+    The target triplet [2,7,5] is now an element of triplets.
 
 Example 2:
 
-Input: triplets = [[3,4,5],[4,5,6]], target = [3,2,5]
-Output: false
-Explanation: It is impossible to have [3,2,5] as an element 
-because there is no 2 in any of the triplets.
+    Input: triplets = [[3,4,5],[4,5,6]], target = [3,2,5]
+
+    Output: false
+
+    Explanation: 
+    
+        It is impossible to have [3,2,5] as an element 
+        because there is no 2 in any of the triplets.
 
 Example 3:
 
-Input: triplets = [[2,5,3],[2,3,4],[1,2,5],[5,2,3]], target = [5,5,5]
+    Input: 
+    
+        triplets = [[2,5,3],[2,3,4],[1,2,5],[5,2,3]], target = [5,5,5]
 
-Output: true
-Explanation: Perform the following operations:
-- Choose the first and third triplets 
-[[2,5,3],[2,3,4],[1,2,5],[5,2,3]]. Update the third triplet to 
-be [max(2,1), max(5,2), max(3,5)] = [2,5,5].
-triplets = [[2,5,3],[2,3,4],[2,5,5],[5,2,3]].
+    Output: true
+    
+    Explanation: 
+        Perform the following operations:
+        
+        - Choose the first and third triplets 
+        [[2,5,3],[2,3,4],[1,2,5],[5,2,3]]. Update the third triplet to 
+        be [max(2,1), max(5,2), max(3,5)] = [2,5,5].
+        triplets = [[2,5,3],[2,3,4],[2,5,5],[5,2,3]].
 
-- Choose the third and fourth triplets 
-[[2,5,3],[2,3,4],[2,5,5],[5,2,3]]. Update the fourth 
-triplet to be [max(2,5), max(5,2), max(5,3)] = [5,5,5]. 
-triplets = [[2,5,3],[2,3,4],[2,5,5],[5,5,5]].
+        - Choose the third and fourth triplets 
+        [[2,5,3],[2,3,4],[2,5,5],[5,2,3]]. Update the fourth 
+        triplet to be [max(2,5), max(5,2), max(5,3)] = [5,5,5]. 
+        triplets = [[2,5,3],[2,3,4],[2,5,5],[5,5,5]].
 
-The target triplet [5,5,5] is now an element of triplets.
+        The target triplet [5,5,5] is now an element of triplets.
 
 Constraints:
 
-    1 <= triplets.length <= 105
+    1 <= triplets.length <= 10^5
+    
     triplets[i].length == target.length == 3
+    
     1 <= ai, bi, ci, x, y, z <= 1000
 
 Takeaway:
 
-Understanding the condition where we can get 
-the target is key
+    Understanding the condition where we can get 
+    the target is key
 
-Some elements are forbidden
+    Some elements are forbidden
 
-Some are the good ones.
-
+    Some are the good ones.
 """
 class Solution:
+    
     def mergeTriplets_(self, triplets: list[list[int]], target: list[int]) -> bool:
         # from a homie 
         
@@ -103,7 +117,7 @@ class Solution:
         return [a, b, c] == target
     
     def mergeTriplets(self, triplets: list[list[int]], target: list[int]) -> bool:
-        # neet
+        # this works
         good = set()
         
         for t in triplets:
