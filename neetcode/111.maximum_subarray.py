5fb35451f998fc3986cbfb7feeea66ba609cca22
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-13 23:09:25 +0300

    The Perfect Hour :stopwatch:

--- neetcode/111.maximum_subarray.py
+++ neetcode/111.maximum_subarray.py
@@ -1,41 +1,47 @@
 """
-Given an integer array nums, find the subarray with the 
-largest sum, and return its sum.
+Given an integer array nums, find the subarray with the largest 
+sum, and return its sum.
 
 Example 1:
 
-Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+    Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
 
-Output: 6
+    Output: 6
 
-Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+    Explanation: The subarray [4,-1,2,1] has the largest sum 6.
 
 Example 2:
 
-Input: nums = [1]
-Output: 1
-Explanation: The subarray [1] has the largest sum 1.
+    Input: nums = [1]
+    
+    Output: 1
+    
+    Explanation: The subarray [1] has the largest sum 1.
 
 Example 3:
 
-Input: nums = [5,4,-1,7,8]
-Output: 23
-Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+    Input: nums = [5,4,-1,7,8]
+    
+    Output: 23
+    
+    Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
 
 Constraints:
 
-1 <= nums.length <= 105
--104 <= nums[i] <= 104
+    1 <= nums.length <= 10^5
+    
+    -10^4 <= nums[i] <= 10^4
 
 Takeaway:
 
-Calculating every single subarray is scary.
+    Calculating every single subarray is scary.
 
-You can use a decision tree to solve 
-this with a sliding window, kinda.
+    You can use a decision tree to solve 
+    this with a sliding window, kinda.
 
 """
 class Solution:
+
     def maxSubArray(self, nums: list[int]) -> int:
         # a decision tree 
         # should we include the current element or not?
@@ -51,7 +57,8 @@ class Solution:
             # [-2,1,-3,4,-1,2,1,-5,4]
             # if the number itself is better than 
             # number added to current sum
-            # it would be WAY BETTER to start a new subarray just using the number
+            # it would be WAY BETTER to start a new subarray just 
+            # using the number
             current_sum = max(num, current_sum + num)
             
             # result changes at every step
