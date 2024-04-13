c26f4890d65ff897b842a380d36e5d0dfea159ae
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2024-04-13 21:58:29 +0300

    The Perfect Hour :stopwatch:

--- neetcode/073.kth_largest_element_in_a_stream.py
+++ neetcode/073.kth_largest_element_in_a_stream.py
@@ -1,65 +1,72 @@
 """
-Design a class to find the kth largest element 
-in a stream. Note that it is the kth 
-largest element in the sorted order, not the 
+Design a class to find the kth largest element in a stream. 
+
+Note that it is the kth largest element in the sorted order, not the 
 kth distinct element.
 
 Implement KthLargest class:
 
     KthLargest(int k, int[] nums) Initializes the object with 
-the integer k and the stream of integers nums.
+        the integer k and the stream of integers nums.
 
     int add(int val) Appends the integer val to the stream 
-and returns the element representing the kth 
-largest element in the stream.
+        and returns the element representing the kth 
+        largest element in the stream.
  
-
 Example 1:
 
-Input
-["KthLargest", "add", "add", "add", "add", "add"]
-[[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
-Output
-[null, 4, 5, 5, 8, 8]
+    Input:
+        ["KthLargest", "add", "add", "add", "add", "add"]
+        [[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+    
+    Output:
+        [null, 4, 5, 5, 8, 8]
 
-Explanation
-KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
-kthLargest.add(3);   // return 4
-kthLargest.add(5);   // return 5
-kthLargest.add(10);  // return 5
-kthLargest.add(9);   // return 8
-kthLargest.add(4);   // return 8
+    Explanation:
+
+        KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+        kthLargest.add(3);   // return 4
+        kthLargest.add(5);   // return 5
+        kthLargest.add(10);  // return 5
+        kthLargest.add(9);   // return 8
+        kthLargest.add(4);   // return 8
  
 Constraints:
 
-1 <= k <= 104
-0 <= nums.length <= 104
--104 <= nums[i] <= 104
--104 <= val <= 104
-At most 104 calls will be made to add.
-It is guaranteed that there will be at least k elements 
-in the array when you search for the kth element.
+    1 <= k <= 10^4
+    
+    0 <= nums.length <= 10^4
+    
+    -10^4 <= nums[i] <= 10^4
+    
+    -10^4 <= val <= 10^4
+    
+    At most 10^4 calls will be made to add.
+    
+    It is guaranteed that there will be at least k elements 
+    
+    in the array when you search for the kth element.
 
 Takeaway:
 
-if we use an array,
-sorting would be n log n
-finding where to insert would be o(n)
+    if we use an array,
+    sorting would be n log n
+    finding where to insert would be o(n)
 
-lets use a min heap of size k
-we can get the min of min heap in o(1)
-we can add an element in log n time
+    lets use a min heap of size k
+    we can get the min of min heap in o(1)
+    we can add an element in log n time
 
-kth element will be the smallest element in 
-size k min heap
-
+    kth element will be the smallest element in 
+    size k min heap
 """
 
 from heapq import heapify, heappush, heappop
 
-# THIS DOES NOT WORK
-# Nice try but yeah.
-class KthLargest:
+
+class KthLargest_:
+    # THIS DOES NOT WORK
+    # Nice try but yeah.
 
     def __init__(self, k: int, nums: "list[int]"):
         """This was an attempt to make MaxHeap
@@ -75,7 +82,6 @@ class KthLargest:
         heappush(self.stream,  (-1) * val)
         #  return kth item on the heap without popping it
         return self.stream[self.k - 1]
-
 
 class KthLargest:
 
